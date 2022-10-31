f8cb583ee783d64a4f2e524dc56c2edf8f28f7b0
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2022-10-31 14:04:03 +0800

    first

--- core/mainListener.cpp
+++ core/mainListener.cpp
@@ -1,12 +1,15 @@
 #include "mainListener.h"
 
+float r = 0, g = 0, b = 0;
+
 void Main::create() {
+	r = g = b = 1;
 	if (!tgf) return;
 	//tgf->clearcolormask(TGF::COLOR_BUFFER_BIT|TGF::DEPTH_BUFFER_BIT|TGF::STENCIL_BUFFER_BIT, 1.f, 0.f, 0.f, 1.f);
 }
 void Main::resume() {
 	if (!tgf) return;
-	
+	r = 1, g = b = 0;
 }
 void Main::resize(unsigned int width, unsigned int height) {
 	if (!tgf) return;
@@ -14,10 +17,11 @@ void Main::resize(unsigned int width, unsigned int height) {
 }
 void Main::render(float delta) {
 	if (!tgf) return;
-	tgf->clearcolormask(TGF::COLOR_BUFFER_BIT|TGF::DEPTH_BUFFER_BIT|TGF::STENCIL_BUFFER_BIT, 0.f, 1.f, 0.f, 1.f);
+	tgf->clearcolormask(TGF::COLOR_BUFFER_BIT|TGF::DEPTH_BUFFER_BIT|TGF::STENCIL_BUFFER_BIT, r, g, b, 1.f);
 	
 }
 void Main::pause() {
+	r = 1, g = 1, b = 0;
 	if (!tgf) return;
 	//tgf->clearcolormask(TGF::COLOR_BUFFER_BIT|TGF::DEPTH_BUFFER_BIT|TGF::STENCIL_BUFFER_BIT, 0.f, 1.f, 0.f, 1.f);
 }
