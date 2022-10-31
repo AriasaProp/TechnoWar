e2b1f778b2164b9f742a2dcc7117d4df1b30d9f0
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2022-10-31 15:22:50 +0800

    first

--- core/mainListener.cpp
+++ core/mainListener.cpp
@@ -3,9 +3,8 @@
 float r = 0, g = 0, b = 0;
 
 void Main::create() {
-	r = g = b = 1;
 	if (!tgf) return;
-	//tgf->clearcolormask(TGF::COLOR_BUFFER_BIT|TGF::DEPTH_BUFFER_BIT|TGF::STENCIL_BUFFER_BIT, 1.f, 0.f, 0.f, 1.f);
+	r = g = b = 1;
 }
 void Main::resume() {
 	if (!tgf) return;
@@ -13,17 +12,16 @@ void Main::resume() {
 }
 void Main::resize(unsigned int width, unsigned int height) {
 	if (!tgf) return;
+	r = g = 0, b = 1;
 	
 }
 void Main::render(float delta) {
 	if (!tgf) return;
 	tgf->clearcolormask(TGF::COLOR_BUFFER_BIT|TGF::DEPTH_BUFFER_BIT|TGF::STENCIL_BUFFER_BIT, r, g, b, 1.f);
-	
 }
 void Main::pause() {
 	r = 1, g = 1, b = 0;
 	if (!tgf) return;
-	//tgf->clearcolormask(TGF::COLOR_BUFFER_BIT|TGF::DEPTH_BUFFER_BIT|TGF::STENCIL_BUFFER_BIT, 0.f, 1.f, 0.f, 1.f);
 }
 void Main::destroy() {
 	if (!tgf) return;
