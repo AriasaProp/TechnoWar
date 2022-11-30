0d0d631a6dad8d785d58104dd26fb017ce7dbc15
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2022-12-01 06:36:21 +0800

    7

--- android/src/main/java/com/ariasaproject/technowar/AndroidApplication.java
+++ android/src/main/java/com/ariasaproject/technowar/AndroidApplication.java
@@ -2,10 +2,14 @@ package com.ariasaproject.technowar;
 
 import android.app.NativeActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.widget.Toast;
+
 
 public class AndroidApplication extends NativeActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGHT_SHORT).show();
   }
 }
