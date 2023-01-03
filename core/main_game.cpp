c9f96114bd30fdac88127053b8ff2630cc0158f2
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-01-03 14:00:02 +0800

    system 7

--- core/main_game.cpp
+++ core/main_game.cpp
@@ -15,18 +15,32 @@ flat_vertex *v_t;
 
 void Main::create(graphics *_graphics, input *_input) {
 	v_t = new flat_vertex[4];
+	unsigned char clr[4];
 	v_t[0].x = 120;
 	v_t[0].y = 120;
-	memcpy(v_t[0].color, ([]unsigned char){0xff,0x00,0x00,0xff}, 4*sizeof(unsigned char));
+	clr[0] = 0xff;
+	clr[1] = 0xff;
+	clr[2] = 0x00;
+	clr[3] = 0xff;
+	memcpy(v_t[0].color, clr, 4*sizeof(unsigned char));
 	v_t[1].x = 120;
 	v_t[1].y = 300;
-	memcpy(v_t[1].color, ([]unsigned char){0xff,0xff,0x00,0xff}, 4*sizeof(unsigned char));
+	clr[0] = 0x00;
+	clr[1] = 0xff;
+	clr[2] = 0x00;
+	memcpy(v_t[1].color, clr, 4*sizeof(unsigned char));
 	v_t[2].x = 300;
 	v_t[2].y = 120;
-	memcpy(v_t[2].color, ([]unsigned char){0x00,0xff,0x00,0xff}, 4*sizeof(unsigned char));
+	clr[0] = 0x00;
+	clr[1] = 0xff;
+	clr[2] = 0xff;
+	memcpy(v_t[2].color, clr, 4*sizeof(unsigned char));
 	v_t[3].x = 300;
 	v_t[3].y = 300;
-	memcpy(v_t[3].color, ([]unsigned char){0x00,0xff,0x00,0xff}, 4*sizeof(unsigned char));
+	clr[0] = 0x00;
+	clr[1] = 0x00;
+	clr[2] = 0xff;
+	memcpy(v_t[3].color, clr, 4*sizeof(unsigned char));
 	m_graphics = _graphics;
 	m_input = _input;
 	mesh_core::data vert[24] = {
@@ -82,7 +96,7 @@ void Main::render(float delta) {
 		M_PI / std::fmax(float(rand()%1000), 240.0f),
 		M_PI / std::fmax(float(rand()%1000), 240.0f)
 	);
-	m_graphics->render_mesh(&mp, 1);
+	m_graphics->mesh_render(&mp, 1);
 	
 	m_graphics->flat_render(v_t, 4);
 }
