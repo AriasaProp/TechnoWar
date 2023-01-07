7f3410ee042b77be42f5deed52e531f8f3d9207f
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-01-07 17:30:08 +0800

    desktop add

--- desktop/desktop_main.cpp
+++ desktop/desktop_main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main () {
     int a=5;     // initial value = 5
-    int b(2);    // initial value = 2
+    int b(3);    // initial value = 2
     int result;  // initial value undetermined
 
     a = a + 4;
