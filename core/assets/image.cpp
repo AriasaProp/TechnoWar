f985569ea89553df8ba4dcb06547262dbef00e37
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-03-28 16:51:53 +0800

    tex-file8

--- core/assets/image.cpp
+++ core/assets/image.cpp
@@ -12,7 +12,7 @@ image::image() {
 }
 image::image(const char *filename) {
 	engine::asset_core *ac = engine::asset->open_asset(filename);
-	const stbi_io_callbacks io {
+	stbi_io_callbacks io {
 		[](void *user, char *data, int size) -> int {
 			return static_cast<engine::asset_core*>(user)->read(data, size);
 		},
