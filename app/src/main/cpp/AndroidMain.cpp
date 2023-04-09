4aff99512fa78012b207f54c67765cbe6c1a4f8f
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-04-09 11:17:47 +0800

    f

--- app/src/main/cpp/AndroidMain.cpp
+++ app/src/main/cpp/AndroidMain.cpp
@@ -1,16 +1,3 @@
-// Copyright 2016 Google Inc. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 #include <android/log.h>
 #include "VulkanMain.hpp"
 
@@ -26,8 +13,7 @@ void handle_cmd(android_app* app, int32_t cmd) {
       DeleteVulkan();
       break;
     default:
-      __android_log_print(ANDROID_LOG_INFO, "Vulkan Tutorials",
-                          "event not handled: %d", cmd);
+      __android_log_print(ANDROID_LOG_INFO, "Vulkan Tutorials", "event not handled: %d", cmd);
   }
 }
 
