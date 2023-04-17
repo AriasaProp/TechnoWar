d13d85c0dd39c904b4c2b12c7857e9c674b1a69b
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-04-17 08:21:34 +0800

    new

--- core/assets/fntRender.cpp
+++ core/assets/fntRender.cpp
@@ -3,9 +3,6 @@
 #include <vector>
 #include <string>
 
-#include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
-
 struct Character {
     int id;
     float x, y, width, height;
@@ -33,55 +30,4 @@ int try_on() {
             characters.push_back(character);
         }
     }
-
-    // Load texture atlas
-    GLuint textureId;
-    glGenTextures(1, &textureId);
-    glBindTexture(GL_TEXTURE_2D, textureId);
-
-    // ... Load texture data ...
-
-    // Set texture parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-    // Setup vertex data
-    float vertices[] = {
-        // Position      TexCoord
-        0.0f, 0.0f,     0.0f, 0.0f,
-        1.0f, 0.0f,     1.0f, 0.0f,
-        1.0f, 1.0f,     1.0f, 1.0f,
-        0.0f, 1.0f,     0.0f, 1.0f
-    };
-
-    GLuint indices[] = {
-        0, 1, 2,
-        0, 2, 3
-    };
-
-    // Setup shader program
-    const char* vertexShaderSource = 
-        "attribute vec4 aPosition;\n"
-        "attribute vec2 aTexCoord;\n"
-        "varying vec2 vTexCoord;\n"
-        "void main() {\n"
-        "  gl_Position = aPosition;\n"
-        "  vTexCoord = aTexCoord;\n"
-        "}\n";
-
-    const char* fragmentShaderSource = 
-        "precision mediump float;\n"
-        "uniform sampler2D uTexture;\n"
-        "varying vec2 vTexCoord;\n"
-        "void main() {\n"
-        "  gl_FragColor = texture2D(uTexture, vTexCoord);\n"
-        "}\n";
-
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1
+}
