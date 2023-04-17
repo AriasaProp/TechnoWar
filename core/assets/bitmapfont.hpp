65dfc04ab0d88607579cc4d3f6f5f5e16df5b80f
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-04-17 22:16:17 +0800

    a

--- core/assets/bitmapfont.hpp
+++ core/assets/bitmapfont.hpp
@@ -18,16 +18,11 @@ struct bitmapfont {
   float glyphHeight;  // tinggi dari setiap karakter pada font
   std::unordered_map<char, Glyph> glyphData;  // data untuk setiap karakter pada font
 	struct glyph {
-	  float left;  // jarak dari posisi karakter ke kiri
-	  float top;  // jarak dari posisi karakter ke atas
-	  float width;  // lebar karakter
-	  float height;  // tinggi karakter
-	  float xoffset;  // perpindahan horizontal dari posisi karakter saat menggambar
-	  float yoffset;  // perpindahan vertikal dari posisi karakter saat menggambar
+	  float x, y;  // jarak dari posisi karakter
+	  float width, height;  // ukuran karakter
+	  float xoffset, yoffset;  // perpindahan posisi karakter saat menggambar
 	  float xadvance;  // jarak horizontal yang ditempuh setelah menggambar karakter
 	};
 };
-
-
 
 #endif //Included_BitmapFont_
