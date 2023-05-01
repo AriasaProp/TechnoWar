6a6bccd258f75dde8d5574aef7368790e68bc79c
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-05-02 06:06:56 +0800

    try to read fnt file

--- core/assets/bitmapfont.hpp
+++ core/assets/bitmapfont.hpp
@@ -3,14 +3,29 @@
 
 #include "../engine.hpp"
 
+struct bitmapfont_set {
+  
+}
 struct bitmapfont {
-private:
-  engine::flat_vertex *quad;
-  engine::texture_core *tex_core;  // ID dari texture yang digunakan untuk menggambar font
 public:
   bitmapfont(const char *, const char *);
   ~bitmapfont();
   void draw();
+  
+private:
+  engine::flat_vertex *quad;
+  engine::texture_core *tex_core;  // ID dari texture yang digunakan untuk menggambar font
+  float fontSize;
+  bool bold, italic;
+  const char *charset;
+  int unicode;
+  float stretchY, stretchX;
+  float smooth, aa, ;
+  float padding_left, padding_top, padding_right, padding_bottom;
+  float spacingX, spacingY;
+  float common_lineHeight, base;
+  float scaleX, scaleY;
+  
 };
 
 #endif //Included_BitmapFont_
