68559e5a1da6526ffd3e4b2f8d2a05c9c597b16e
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-05-11 13:27:20 +0800

    fix insets

--- android/src/main/java/com/ariasaproject/technowar/MainActivity.java
+++ android/src/main/java/com/ariasaproject/technowar/MainActivity.java
@@ -7,45 +7,26 @@ import android.view.View;
 import android.view.DisplayCutout;
 import android.view.WindowInsets;
 
-public class MainActivity extends NativeActivity {
+public class MainActivity extends NativeActivity implements  View.OnApplyWindowInsetsListener {
     static native void setInsets(int left, int top, int right, int bottom);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //get insets
-    		if (Build.VERSION.SDK_INT >= 28) {
-            final View decorView = getWindow().getDecorView();
-            decorView.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
-                    private DisplayCutout mDisplayCutout;
-                    @Override
-                    public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
-                        mDisplayCutout = insets.getDisplayCutout();
-                        setInsets(
-                          mDisplayCutout.getSafeInsetLeft(),
-                          mDisplayCutout.getSafeInsetTop(),
-                          mDisplayCutout.getSafeInsetRight(),
-                          mDisplayCutout.getSafeInsetBottom());
-                        return insets;
-                    }
-                });
-    		}
+        getWindow().getDecorView().setOnApplyWindowInsetsListener(this);
     }
+    private DisplayCutout mDisplayCutout;
     @Override
-    protected void onStart() {
-    		if (Build.VERSION.SDK_INT >= 28) {
-            final View decorView = getWindow().getDecorView();
-            final DisplayCutout displayCutout = decorView.getRootWindowInsets().getDisplayCutout();
-            if (displayCutout != null) {
-                setInsets(
-                  displayCutout.getSafeInsetLeft(),
-                  displayCutout.getSafeInsetTop(),
-                  displayCutout.getSafeInsetRight(),
-                  displayCutout.getSafeInsetBottom()
-                );
-            }
-    		}
-    		super.onStart();
+    public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+        if (Build.VERSION.SDK_INT < 28) return;
+        mDisplayCutout = insets.getDisplayCutout();
+        setInsets(
+          mDisplayCutout.getSafeInsetLeft(),
+          mDisplayCutout.getSafeInsetTop(),
+          mDisplayCutout.getSafeInsetRight(),
+          mDisplayCutout.getSafeInsetBottom());
+        return insets;
     }
+    
     
 }
