44324afe50d69e29daeed638047d4c16314712b5
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-05-11 14:57:13 +0000

    reformating cpp

--- core/main_game.cpp
+++ core/main_game.cpp
@@ -16,12 +16,12 @@ engine::texture_core *myTex;
 bmfont *fnt;
 
 Main::Main () {
-  fnt = new bmfont("default.fnt");
+  fnt = new bmfont ("default.fnt");
   int x, y;
   unsigned int datRI;
-  void *datR = engine::asset->asset_buffer("test.jpeg", &datRI);
-  unsigned char *tD = stbi_load_from_memory ((unsigned char const*)datR, (int)datRI, &x, &y, nullptr, STBI_rgb_alpha);
-  free(datR);
+  void *datR = engine::asset->asset_buffer ("test.jpeg", &datRI);
+  unsigned char *tD = stbi_load_from_memory ((unsigned char const *)datR, (int)datRI, &x, &y, nullptr, STBI_rgb_alpha);
+  free (datR);
   myTex = engine::graph->gen_texture (x, y, tD);
   stbi_image_free (tD);
   engine::mesh_core::data vert[24] = {
@@ -54,16 +54,9 @@ Main::Main () {
       {-350.0f, +350.0f, +350.0f, 0x00, 0xff, 0xff, 0xff},
       {-350.0f, -350.0f, +350.0f, 0xff, 0xff, 0xff, 0xff},
       {+350.0f, -350.0f, +350.0f, 0x00, 0x00, 0xff, 0xff},
-      {+350.0f, +350.0f, +350.0f, 0x00, 0xff, 0x00, 0xff}
-  };
+      {+350.0f, +350.0f, +350.0f, 0x00, 0xff, 0x00, 0xff}};
   unsigned short indices[36] = {
-    0, 1, 3, 1, 2, 3,
-    4, 5, 7, 5, 6, 7,
-    8, 9, 11, 9, 10, 11,
-    12, 13, 15, 13, 14, 15,
-    16, 17, 19, 17, 18, 19,
-    20, 21, 23, 21, 22, 23
-  };
+      0, 1, 3, 1, 2, 3, 4, 5, 7, 5, 6, 7, 8, 9, 11, 9, 10, 11, 12, 13, 15, 13, 14, 15, 16, 17, 19, 17, 18, 19, 20, 21, 23, 21, 22, 23};
   mp = engine::graph->gen_mesh (vert, 24, indices, 36);
   fV = new engine::flat_vertex[4]{
       {120.f, 120.f, {0xff, 0xf0, 0x01, 0xff}, 0, 1},
@@ -78,10 +71,10 @@ Main::Main () {
 }
 void Main::resume () {
 }
-std::chrono::time_point<std::chrono::high_resolution_clock> startP = std::chrono::high_resolution_clock::now(), endP;
+std::chrono::time_point<std::chrono::high_resolution_clock> startP = std::chrono::high_resolution_clock::now (), endP;
 float delta = 0;
 void Main::render () {
-  endP = std::chrono::high_resolution_clock::now();
+  endP = std::chrono::high_resolution_clock::now ();
   delta = float (std::chrono::duration_cast<std::chrono::microseconds> (endP - startP).count ()) / 1000000.f;
   startP = endP;
   engine::inpt->process_event ();
@@ -95,9 +88,9 @@ void Main::render () {
   engine::graph->mesh_render (&mp, 1);
   engine::graph->flat_render (myTex, fV, 1);
   engine::graph->flat_render (myTex, fV1, 1);
-  fnt->draw_text(0+10, engine::graph->getHeight(), ALIGN_TOP_LEFT, "Left Side!");
-  fnt->draw_text(engine::graph->getWidth()*0.5f, engine::graph->getHeight(), ALIGN_TOP, "Middle Side!");
-  fnt->draw_text(engine::graph->getWidth()-10, engine::graph->getHeight(), ALIGN_TOP_RIGHT, "Right Side!");
+  fnt->draw_text (0 + 10, engine::graph->getHeight (), ALIGN_TOP_LEFT, "Left Side!");
+  fnt->draw_text (engine::graph->getWidth () * 0.5f, engine::graph->getHeight (), ALIGN_TOP, "Middle Side!");
+  fnt->draw_text (engine::graph->getWidth () - 10, engine::graph->getHeight (), ALIGN_TOP_RIGHT, "Right Side!");
 }
 void Main::pause () {
 }
