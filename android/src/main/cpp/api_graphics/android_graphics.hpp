02e0087952a3184a52c286203374ee96eb55c4cf
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-05-29 13:20:41 +0800

    pre and post render 2

--- android/src/main/cpp/api_graphics/android_graphics.hpp
+++ android/src/main/cpp/api_graphics/android_graphics.hpp
@@ -12,13 +12,11 @@
 struct android_graphics : public engine::graphics_core {
   float cur_safe_insets[4];
   // android
-  virtual void onResume () = 0;
   virtual void onWindowInit (ANativeWindow *) = 0;
   virtual bool preRender (unsigned int&) = 0;
   virtual void render (unsigned int&) = 0;
   virtual void postRender (bool) = 0;
   virtual void onWindowTerm () = 0;
-  virtual void onPause () = 0;
   virtual ~android_graphics () = 0;
 };
 
