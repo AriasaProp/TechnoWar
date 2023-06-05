e960269b145a91c86031de5420584dfd17024c8a
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-06-05 12:40:20 +0800

    test uistage d1

--- core/utils/uistage.cpp
+++ core/utils/uistage.cpp
@@ -63,52 +63,52 @@ void uistage::draw () {
     //bottom - left
     *(verts++) = {x1, y1, a.color, 0, 1};
     if (patched) {
-      *(verts++) = {x1, y1+split[3], a.color, 0, 1-float(split[3]/ta.size[1])};
-      *(verts++) = {x1+split[0], y1, a.color, float(split[0]/ta.size[0]), 1};
-      *(verts++) = {x1+split[0], y1+split[3], a.color, float(split[0]/ta.size[0]), 1-float(split[3]/ta.size[1])};
+      *(verts++) = {x1, y1+split[3], a.color, 0, 1-float(split[3]/ta.region.size[1])};
+      *(verts++) = {x1+split[0], y1, a.color, float(split[0]/ta.region.size[0]), 1};
+      *(verts++) = {x1+split[0], y1+split[3], a.color, float(split[0]/ta.region.size[0]), 1-float(split[3]/ta.region.size[1])};
     //center - left
-      *(verts++) = {x1, y1+split[3], a.color, 0, 1-float(split[3]/ta.size[1])};
-      *(verts++) = {x1, y2-split[1], a.color, 0, float(split[1]/ta.size[1])};
-      *(verts++) = {x1+split[0], y1+split[3], a.color, float(split[0]/ta.size[0]), 1-float(split[3]/ta.size[1])};
-      *(verts++) = {x1+split[0], y2-split[1], a.color, float(split[0]/ta.size[0]), float(split[1]/ta.size[1])};
+      *(verts++) = {x1, y1+split[3], a.color, 0, 1-float(split[3]/ta.region.size[1])};
+      *(verts++) = {x1, y2-split[1], a.color, 0, float(split[1]/ta.region.size[1])};
+      *(verts++) = {x1+split[0], y1+split[3], a.color, float(split[0]/ta.region.size[0]), 1-float(split[3]/ta.region.size[1])};
+      *(verts++) = {x1+split[0], y2-split[1], a.color, float(split[0]/ta.region.size[0]), float(split[1]/ta.region.size[1])};
     //top - left
-      *(verts++) = {x1, y2-split[1], a.color, 0, float(split[1]/ta.size[1])};
+      *(verts++) = {x1, y2-split[1], a.color, 0, float(split[1]/ta.region.size[1])};
     }
     *(verts++) = {x1, y2, a.color, 0, 0};
     if (patched) {
-      *(verts++) = {x1+split[0], y2-split[1], a.color, float(split[0]/ta.size[0]), float(split[1]/ta.size[1])};
-      *(verts++) = {x1+split[0], y2, a.color, float(split[0]/ta.size[0]), 0};
+      *(verts++) = {x1+split[0], y2-split[1], a.color, float(split[0]/ta.region.size[0]), float(split[1]/ta.region.size[1])};
+      *(verts++) = {x1+split[0], y2, a.color, float(split[0]/ta.region.size[0]), 0};
     //bottom - center
-      *(verts++) = {x1+split[0], y1, a.color, float(split[0]/ta.size[0]), 1};
-      *(verts++) = {x1+split[0], y1+split[3], a.color, float(split[0]/ta.size[0]), 1-float(split[3]/ta.size[1])};
-      *(verts++) = {x2-split[2], y1, a.color, 1-float(split[1]/ta.size[0]), 1};
-      *(verts++) = {x2-split[2], y1+split[3], a.color, 1-float(split[1]/ta.size[0]), 1-float(split[3]/ta.size[1])};
+      *(verts++) = {x1+split[0], y1, a.color, float(split[0]/ta.region.size[0]), 1};
+      *(verts++) = {x1+split[0], y1+split[3], a.color, float(split[0]/ta.region.size[0]), 1-float(split[3]/ta.region.size[1])};
+      *(verts++) = {x2-split[2], y1, a.color, 1-float(split[1]/ta.region.size[0]), 1};
+      *(verts++) = {x2-split[2], y1+split[3], a.color, 1-float(split[1]/ta.region.size[0]), 1-float(split[3]/ta.region.size[1])};
     //center
-      *(verts++) = {x1+split[0], y1+split[3], a.color, float(split[0]/ta.size[0]), 1-float(split[3]/ta.size[1])};
-      *(verts++) = {x1+split[0], y2-split[1], a.color, float(split[0]/ta.size[0]), float(split[1]/ta.size[1])};
-      *(verts++) = {x2-split[2], y1+split[3], a.color, 1-float(split[2]/ta.size[0]), 1-float(split[3]/ta.size[1])};
-      *(verts++) = {x2-split[2], y2-split[1], a.color, 1-float(split[2]/ta.size[0]), float(split[1]/ta.size[1])};
+      *(verts++) = {x1+split[0], y1+split[3], a.color, float(split[0]/ta.region.size[0]), 1-float(split[3]/ta.region.size[1])};
+      *(verts++) = {x1+split[0], y2-split[1], a.color, float(split[0]/ta.region.size[0]), float(split[1]/ta.region.size[1])};
+      *(verts++) = {x2-split[2], y1+split[3], a.color, 1-float(split[2]/ta.region.size[0]), 1-float(split[3]/ta.region.size[1])};
+      *(verts++) = {x2-split[2], y2-split[1], a.color, 1-float(split[2]/ta.region.size[0]), float(split[1]/ta.region.size[1])};
     //top - center
-      *(verts++) = {x1+split[0], y2-split[1], a.color, float(split[0]/ta.size[0]), float(split[1]/ta.size[1])};
-      *(verts++) = {x1+split[0], y2, a.color, float(split[0]/ta.size[0]), 0};
-      *(verts++) = {x2-split[2], y2-split[1], a.color, 1-float(split[2]/ta.size[0]), float(split[1]/ta.size[1])};
-      *(verts++) = {x2-split[2], y2, a.color, 1-float(split[2]/ta.size[0]), 0};
+      *(verts++) = {x1+split[0], y2-split[1], a.color, float(split[0]/ta.region.size[0]), float(split[1]/ta.region.size[1])};
+      *(verts++) = {x1+split[0], y2, a.color, float(split[0]/ta.region.size[0]), 0};
+      *(verts++) = {x2-split[2], y2-split[1], a.color, 1-float(split[2]/ta.region.size[0]), float(split[1]/ta.region.size[1])};
+      *(verts++) = {x2-split[2], y2, a.color, 1-float(split[2]/ta.region.size[0]), 0};
     //bottom - right
-      *(verts++) = {x2-split[2], y1, a.color, 1-float(split[1]/ta.size[0]), 1};
-      *(verts++) = {x2-split[2], y1+split[3], a.color, 1-float(split[1]/ta.size[0]), 1-float(split[3]/ta.size[1])};
+      *(verts++) = {x2-split[2], y1, a.color, 1-float(split[1]/ta.region.size[0]), 1};
+      *(verts++) = {x2-split[2], y1+split[3], a.color, 1-float(split[1]/ta.region.size[0]), 1-float(split[3]/ta.region.size[1])};
     }
     *(verts++) = {x2, y1, a.color, 1, 1};
     if (patched) {
-      *(verts++) = {x2, y1+split[3], a.color, 1, 1-float(split[3]/ta.size[1])};
+      *(verts++) = {x2, y1+split[3], a.color, 1, 1-float(split[3]/ta.region.size[1])};
     //center - right
-      *(verts++) = {x2-split[2], y1+split[3], a.color, 1-float(split[1]/ta.size[0]), 1-float(split[3]/ta.size[1])};
-      *(verts++) = {x2-split[2], y2-split[1], a.color, 1-float(split[1]/ta.size[0]), float(split[1]/ta.size[1])};
-      *(verts++) = {x2, y1+split[3], a.color, 1, 1-float(split[3]/ta.size[1])};
-      *(verts++) = {x2, y2-split[1], a.color, 1, float(split[1]/ta.size[1])};
+      *(verts++) = {x2-split[2], y1+split[3], a.color, 1-float(split[1]/ta.region.size[0]), 1-float(split[3]/ta.region.size[1])};
+      *(verts++) = {x2-split[2], y2-split[1], a.color, 1-float(split[1]/ta.region.size[0]), float(split[1]/ta.region.size[1])};
+      *(verts++) = {x2, y1+split[3], a.color, 1, 1-float(split[3]/ta.region.size[1])};
+      *(verts++) = {x2, y2-split[1], a.color, 1, float(split[1]/ta.region.size[1])};
     //top - right
-      *(verts++) = {x2-split[2], y2-split[1], a.color, 1-float(split[1]/ta.size[0]), float(split[1]/ta.size[1])};
-      *(verts++) = {x2-split[2], y2, a.color, 1-float(split[1]/ta.size[0]), 0};
-      *(verts++) = {x2, y2-split[1], a.color, 1, float(split[1]/ta.size[1])};
+      *(verts++) = {x2-split[2], y2-split[1], a.color, 1-float(split[1]/ta.region.size[0]), float(split[1]/ta.region.size[1])};
+      *(verts++) = {x2-split[2], y2, a.color, 1-float(split[1]/ta.region.size[0]), 0};
+      *(verts++) = {x2, y2-split[1], a.color, 1, float(split[1]/ta.region.size[1])};
     }
     *(verts++) = {x2, y2, a.color, 1, 0};
     
