9d42ee653b45173d2334fbe1873378224afb52ef
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-06-08 22:22:57 +0800

    fix uistage 4

--- core/utils/uistage.cpp
+++ core/utils/uistage.cpp
@@ -33,42 +33,52 @@ void uistage::act (float) {
   //nothing to do
 }
 static engine::flat_vertex vert[1024];
+static engine::flat_vertex v_;
+static float cList[4];
+static float vList[4];
+static float rList[4];
+static float uList[4];
 void uistage::draw () {
-  float xmin, ymin, xmax, ymax;
-  float umin, vmin, umax, vmax;
-  engine::flat_vertex v_;
-  for (auto i = actors.begin(), j = actors.end(); i != j; i++) {
+  for (actor *act : actors) {
     engine::flat_vertex *verts = vert;
-    actor &a = **i;
-    textureAtlas &ta = regions[a.texKey];
+    textureAtlas &ta = regions[act->texKey];
     engine::texture_core *tex = ta.tex;
     // left, top, right, bottom
     const unsigned int *split = ta.region.patch;
+    memcpy(&v_.color, act->color, sizeof(v_.color));
+    cList[0] = act->pos[1];
+    cList[3] = cList[0] + act->size[1];
+    cList[1] = cList[0] + split[3];
+    cList[2] = cList[3] - split[1];
     
-    memcpy(&v_.color, a.color, sizeof(v_.color));
-    ymin = a.pos[1], ymax = ymin+a.size[1];
-    xmax = xmin+a.size[0];
-    umax = float(ta.region.pos[0]+ta.region.size[0])/float(tex->width());
-    vmin = float(ta.region.pos[1]+ta.region.size[1])/float(tex->height());
-    vmax = float(ta.region.pos[1])/float(tex->height());
+    vList[0] = float(ta.region.pos[1]+ta.region.size[1])/float(tex->height());
+    vList[3] = float(ta.region.pos[1])/float(tex->height());
+    vList[1] = float(ta.region.pos[1]+ta.region.size[1]-split[3])/float(tex->height());
+    vList[2] = float(ta.region.pos[1]+split[1])/float(tex->height());
     
-    const float columnList[4]{ymin ,ymin+split[3], ymax-split[1], ymax};
-    const float vList[4]{vmin ,vmin-float(split[3])/float(tex->height()), vmax+float(split[1])/float(tex->height()), vmax};
-    const float rowList[4]{xmin ,xmin+split[0], xmax-split[2], xmax};
-    const float uList[4]{umin ,umin+float(split[0])/float(tex->width()), umax-float(split[2])/float(tex->width()), umax};
+    rList[0] = act->pos[0];
+    rList[3] = rList[0] + act->size[0];
+    rList[1] = rList[0] + split[0];
+    rList[2] = rList[3] - split[2];
+    
+    uList[0] = float(ta.region.pos[0])/float(tex->width());
+    uList[3] = float(ta.region.pos[0]+ta.region.size[0])/float(tex->width());
+    uList[1] = float(ta.region.pos[0]+split[0])/float(tex->width())
+    uList[2] = float(ta.region.pos[0]+ta.region.size[0]-split[2])/float(tex->width());
+    
     size_t quadCount = 0;
-    for (size_t i = 0; i < 3; i++) { //vertical list
-      ymin = columnList[i];
-      ymax = columnList[i+1];
+    for (size_t p = 0; p < 3; p++) { //vertical list
+      float &ymin  = cList[p];
+      float &ymax = cList[p+1];
       if (ymax == ymin) continue;
-      vmin = vList[i];
-      vmax = vList[i+1];
-      for (size_t j = 0; j < 3; j++) { //horizontal list
-        xmin = rowList[j];
-        xmax = rowList[j+1];
+      float &vmin = vList[p];
+      float &vmax = vList[p+1];
+      for (size_t q = 0; q < 3; q++) { //horizontal list
+        float &xmin = rList[q];
+        float &xmax = rList[q+1];
         if (xmax == xmin) continue;
-        umin = uList[j];
-        umax = uList[j+1];
+        float &umin = uList[q];
+        float &umax = uList[q+1];
         v_.x = xmin, v_.u = umin;
         v_.y = ymin, v_.v = vmin;
         memcpy(verts++, &v_, sizeof(v_));
