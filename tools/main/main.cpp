32764445cf7e788dd1d1860475843b44ef49c48d
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-10-22 00:08:23 +0800

    add tools

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -1,6 +1,50 @@
-#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <string>
 
-int main(int argv, char **args) {
-  
-  printf("Hello World");
+char buffer[2048];
+int main(int argc, char** argv) {
+    std::string inputFileName;
+    std::string outputFileName;
+
+    for (int i = 1; i < argc; i++) {
+      if (i + 1 < argc) {
+        std::string a(argv[i]);
+        if (a == "-i") {
+            inputFileName = argv[i + 1];
+        } else if (a == "-o") {
+            outputFileName = argv[i + 1];
+        } else {
+            std::cout << "Unknow arguments of " << a << std::endl;
+        }
+      }
+    }
+
+    if (inputFileName.empty() || outputFileName.empty()) {
+        std::cerr << "Usage: " << argv[0] << " -i <input file> -o <output file>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream inputFile(inputFileName, std::ios::binary); // Open input file in binary mode
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Could not open input file." << std::endl;
+        return 1;
+    }
+
+    std::ofstream outputFile(outputFileName, std::ios::binary); // Open output file in binary mode
+    if (!outputFile.is_open()) {
+        std::cerr << "Error: Could not open output file." << std::endl;
+        return 1;
+    }
+    while (!inputFile.eof()) {
+        inputFile.read(buffer, sizeof(buffer));
+        outputFile.write(buffer, inputFile.gcount());
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    std::cout << "File conversion complete." << std::endl;
+
+    return 0;
 }
