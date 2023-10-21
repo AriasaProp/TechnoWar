b7a18aae2901026f3291718e5126f69902eb80ca
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-10-21 21:32:03 +0800

    add tools

---
+++ tools/main/cmake/CMakeFiles/Converter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Converter.dir/main.cpp.o"
+  "CMakeFiles/Converter.dir/main.cpp.o.d"
+  "Converter"
+  "Converter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Converter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
