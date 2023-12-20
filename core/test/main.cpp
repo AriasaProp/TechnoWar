125fd9f4710763654c871d6c84ff926d69d68493
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-12-20 23:03:12 +0800

    add test then check

---
+++ core/test/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Test for Core module" << std::endl;
+	return 0;
+}
---
+++ tools/test/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Test for Tools module" << std::endl;
+	return 0;
+}
