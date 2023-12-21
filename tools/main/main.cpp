0b9083aa8139fd5aef06d3f980c1071511eccc86
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-12-21 20:28:27 +0800

    measure error in open

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cstring>
 #include <cstdlib>
+#include <stdexcept>
 
 char buffer[4096];
 int main(int argc, char** argv) {
@@ -17,32 +18,29 @@ int main(int argc, char** argv) {
       } else if (a == "-o") {
         outputFileName = std::string(argv[i + 1]);
       } else {
-        throw std::string("Unknow arguments of ") + a;
+        throw std::runtime_error("Unknow arguments of " + a);
       }
     }
     
     if (inputFileName.empty() || outputFileName.empty())
-      throw "Input empty";
-  	try {
-	    std::ifstream inputFile(inputFileName, std::ios::binary);
-	    if (!inputFile.is_open())
-	      throw "Could not open input file.";
-    	std::ofstream outputFile(outputFileName, std::ios::binary | std::ios::out | std::ios::trunc);
-	    if (!outputFile.is_open())
-	      throw "Could not open output file.";
-	    while (!inputFile.eof()) {
-	      inputFile.read(buffer, sizeof(buffer));
-	      outputFile.write(buffer, inputFile.gcount());
-	    }
-	  
-	    inputFile.close();
-	    outputFile.close();
-  	} catch (const char *err) { throw err;
-  	} catch (...) { throw "Unknow file proccess error."; }
+      throw  std::runtime_error("Input empty");
+    std::ifstream inputFile(inputFileName, std::ios::binary);
+    if (!inputFile.is_open())
+      throw  std::runtime_error("Could not open input file.");
+  	std::ofstream outputFile(outputFileName, std::ios::binary | std::ios::out | std::ios::trunc);
+    if (!outputFile.is_open())
+      throw  std::runtime_error("Could not open output file.");
+    while (!inputFile.eof()) {
+      inputFile.read(buffer, sizeof(buffer));
+      outputFile.write(buffer, inputFile.gcount());
+    }
+  
+    inputFile.close();
+    outputFile.close();
   
     std::cout << "File conversion complete." << std::endl;
-  } catch (std::string err) {
-    std::cout << "Error: " << err << std::endl;
+  } catch (std::exception err) {
+    std::cout << "Error: " << err.what() << std::endl;
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
