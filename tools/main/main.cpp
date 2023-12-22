6024f0aa73e03c5c23dbc408744a7f326d5e8a9c
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-12-22 08:48:25 +0800

    low input

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -11,13 +11,13 @@ int main(int argc, char** argv) {
   	if (argc < 3) throw  std::runtime_error("Input empty");
     std::string ifn = std::string(argv[1]);
     std::string ofn = std::string(argv[2]);
-    /*
     if (inputFileName.empty() || outputFileName.empty())
       throw  std::runtime_error("Input empty");
     std::ifstream inputFile(inputFileName, std::ios::binary);
+  	std::ofstream outputFile(outputFileName, std::ios::binary | std::ios::out | std::ios::trunc);
+    /*
     if (!inputFile.is_open())
       throw  std::runtime_error("Could not open input file.");
-  	std::ofstream outputFile(outputFileName, std::ios::binary | std::ios::out | std::ios::trunc);
     if (!outputFile.is_open())
       throw  std::runtime_error("Could not open output file.");
     while (!inputFile.eof()) {
@@ -25,15 +25,15 @@ int main(int argc, char** argv) {
       outputFile.write(buffer, inputFile.gcount());
     }
   
+  	*/
     inputFile.close();
     outputFile.close();
-  	*/
     std::cout << "in " << ifn << " & out " << ofn << std::endl;
     std::cout << "File conversion complete." << std::endl;
     return EXIT_FAILURE;
   } catch (std::exception err) {
     std::cout << "Error: " << err.what() << std::endl;
-    //return EXIT_FAILURE;
+    return EXIT_FAILURE;
   }
-  return EXIT_SUCCESS;
+  //return EXIT_SUCCESS;
 }
