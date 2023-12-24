f63cf11e27f9cdd7989463ee3c5739a9c0e8c588
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-12-25 07:55:09 +0800

    add size info

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char** argv) {
 	    unsigned char *inpBuffer = stbi_load_from_callbacks(&sic_file, (void*)&ifile, (int*)dat[0], (int*)dat[1], (int*)dat[2], STBI_rgb_alpha);
 	    if (dat[2] == STBI_rgb_alpha) {
 		    ofile.write((char*)dat, sizeof(unsigned int)*2);
-		    ofile.write((char*)inpBuffer, x*y*comp);
+		    ofile.write((char*)inpBuffer, dat[0]*dat[1]*dat[3]);
 		    stbi_image_free(inpBuffer);
 	    }
 	    ifile.close();
