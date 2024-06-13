d48acac09a9f463b7887a9add5458a157880ce55
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-06-13 17:21:02 +0000

    Core Reformatting Sources

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -2,15 +2,15 @@
 #include "stb_image_write.hpp"
 #include "stb_rect_pack.hpp"
 
+#include <cstdlib>
 #include <cstring>
 #include <fstream>
 #include <iostream>
-#include <cstdlib>
 #include <string>
 
 int main (int argc, char *argv[]) {
   try {
-  	std::cout << "Converting ... " << std::endl;
+    std::cout << "Converting ... " << std::endl;
     if (argc < 3) throw "Input empty";
     if (!argv[1] || !argv[1][0] || !argv[2] || !argv[2][0])
       throw "Input empty";
@@ -18,17 +18,16 @@ int main (int argc, char *argv[]) {
     int dat[3];
     unsigned char *inpBuffer = stbi::load (argv[1], &dat[0], &dat[1], &dat[2], stbi::channel::rgb_alpha);
     if (!inpBuffer)
-    	throw stbi::failure_reason ();
+      throw stbi::failure_reason ();
     if (dat[2] != stbi::channel::rgb_alpha)
-    	throw "failed convert channels";
-    stbi::write::png(argv[2], dat[0], dat[1], dat[2], (void*)inpBuffer, 0);
+      throw "failed convert channels";
+    stbi::write::png (argv[2], dat[0], dat[1], dat[2], (void *)inpBuffer, 0);
     stbi::image_free (inpBuffer);
     std::cout << argv[2] << " completed." << std::endl;
-	  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
   } catch (const char *err) {
     std::cout << " Error: " << err;
     std::cout << std::endl;
     return EXIT_FAILURE;
   }
-  
 }
