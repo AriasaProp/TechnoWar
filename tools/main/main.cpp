c6c8637ca6efd7094cccffc317a84b72b54c0353
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-06-15 22:16:29 +0800

    fix in 22:16 15/06/24

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -1,17 +1,19 @@
 #include "stbi_load.hpp"
-#include "stbi_rectpack.hpp"
 #include "stbi_write.hpp"
+#include "stbi_rectpack.hpp"
 
 #include <cstdlib>
 #include <cstring>
-#include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <set>
 #include <string>
+#include <filesystem>
 #include <vector>
+#include <set>
 
 namespace fs = std::filesystem;
+
+#define PACKED_SIZE 4096
 
 int main (int argc, char *argv[]) {
   try {
@@ -20,64 +22,43 @@ int main (int argc, char *argv[]) {
       throw "Input empty";
     std::cout << "Input folder: " << argv[1] << std::endl;
     static const std::set<std::string> exten = {".png", ".jpg", ".jpeg", ".bmp", ".tga", ".gif"};
-    for (const auto &entry : fs::directory_iterator (argv[1])) {
-      if ((!fs::is_regular_file (entry.status ())) || (exten.find (entry.path ().extension ().string ()) == exten.end ())) continue;
-      int dat[3];
-      stbi::load::info (entry.path ().c_str (), dat, dat + 1, dat + 2);
-      std::cout << "Size: " << dat[0] << " x " << dat[1] << ", Channel: ";
-      switch (dat[2]) {
-      default:
-      case stbi::load::channel::none:
-        std::cout << "None?";
-        break;
-      case stbi::load::channel::grey:
-        std::cout << "Grey";
-        break;
-      case stbi::load::channel::grey_alpha:
-        std::cout << "Grey_Alpha";
-        break;
-      case stbi::load::channel::rgb:
-        std::cout << "RGB";
-        break;
-      case stbi::load::channel::rgb_alpha:
-        std::cout << "RGBA";
-        break;
-      }
-      std::cout << std::endl;
-    }
-    /*
-    unsigned char *inpBuffer = stbi::load::load_from_filename (argv[1], &dat[0], &dat[1], &dat[2], stbi::load::channel::rgb_alpha);
-    if (!inpBuffer)
-      throw "Input file error";
-    if (dat[2] != stbi::load::channel::rgb_alpha) {
-      std::cout << "Channel out is: ";
-      switch (dat[2]) {
-      default:
-      case stbi::load::channel::none:
-        std::cout << "None?";
-        break;
-      case stbi::load::channel::grey:
-        std::cout << "Grey";
-        break;
-      case stbi::load::channel::grey_alpha:
-        std::cout << "Grey_Alpha";
-        break;
-      case stbi::load::channel::rgb:
-        std::cout << "RGB";
-        break;
-      case stbi::load::channel::rgb_alpha:
-        std::cout << "Huh?";
-        break;
-      }
-      std::cout << std::endl;
-    }
-    std::cout << "Output file: " << argv[2] << std::endl;
-    stbi::write::png (argv[2], dat[0], dat[1], dat[2], (void *)inpBuffer, 0);
-    stbi::load::image_free (inpBuffer);
-    */
+    stbi::rectpack::context p_context;
+    stbi::rectpack::node p_nodes[STBRP__MAXVAL];
+    stbi::rectpack::init_target(&p_context, PACKED_SIZE, PACKED_SIZE, p_nodes, STBRP__MAXVAL)
+		std::vector<stbi::rectpack::rect> rects;
+		for (const auto& entry : fs::directory_iterator(argv[1])) {
+      if ((!fs::is_regular_file(entry.status())) || (exten.find(entry.path().extension().string()) == exten.end())) continue;
+    	static int dat[3];
+      stbi::load::info(entry.path().c_str(), dat, dat+1, dat+2);
+      std::cout << "Size: " << dat[0] << " x " << dat[1] << ", Channel is ";
+      if (dat[2] != stbi::load::channel::rgb_alpha) {
+      	std::cout << "not RGBA" << std::endl;
+      	continue;
+    	}
+    	std::cout << "RGBA" << std::endl;
+    	rects.push_back({(void*)new std::string(entry.path().string()), dat[0], dat[1], 0, 0, 0});
+		}
+		if (!stbi::rectpack::pack_rects(&p_context, rects.data(), rects.size())) throw "All not packed!"
+		unsigned char *outBuffer = new unsigned char[PACKED_SIZE*PACKED_SIZE*4];
+		for (stbi::rectpack::rect r : rects) {
+			static int dat[3];
+			std::string *iname = static_cast<std::string*>(r.id);
+    	unsigned char *inpBuffer = stbi::load::load_from_filename (iname->c_str(), dat, dat + 1, dat + 2, stbi::load::channel::rgb_alpha);
+	    if (inpBuffer) {
+	    	for (unsigned y = 0; y < dat[1]; y++) {
+	    		memcpy(outBuffer+(r.y*dat[0]*4)+(r.x*4), inpBuffer+(y*dat[0]), dat[0]*4);
+	    	}
+				stbi::load::image_free (inpBuffer);
+	    } else {{
+	    	std::cout << "failed an image, " << *iname << std::endl;
+	    }
+			delete iname;
+		}
+  	stbi::write::png (argv[2], PACKED_SIZE, PACKED_SIZE, stbi::load::channel::rgb_alpha, outBuffer, 0);
+		delete[] outBuffer;
     std::cout << "Output: " << argv[2] << " completed." << std::endl;
   } catch (const fs::filesystem_error &e) {
-    std::cerr << "Error reading directory: " << e.what () << std::endl;
+    std::cerr << "Error reading directory: " << e.what() << std::endl;
     return EXIT_FAILURE;
   } catch (const char *err) {
     std::cout << " Error: " << err << std::endl;
@@ -86,3 +67,4 @@ int main (int argc, char *argv[]) {
   }
   return EXIT_SUCCESS;
 }
+
