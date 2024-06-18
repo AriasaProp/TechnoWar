2ba79b5a033b3724ec467d22c07752f4f658b1a7
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-06-18 01:57:28 +0000

    Core Reformatting Sources

--- tools/test/main.cpp
+++ tools/test/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-extern bool own_rectpack_test();
+extern bool own_rectpack_test ();
 
 int main () {
-	bool passed = true;
+  bool passed = true;
   std::cout << "Start Test - Tools module" << std::endl;
-  passed &= own_rectpack_test();
+  passed &= own_rectpack_test ();
   std::cout << "End Test - Tools module" << std::endl;
   return 0;
 }
--- tools/test/own_rectpack_test.cpp
+++ tools/test/own_rectpack_test.cpp
@@ -1,14 +1,14 @@
 #include "own_rectpack.hpp"
 
+#include <cstring>
 #include <iostream>
 #include <vector>
-#include <cstring>
 
-bool own_rectpack_test() {
+bool own_rectpack_test () {
   std::cout << "Start Test - Rect pack" << std::endl;
-	unsigned int binWidth = 64;
+  unsigned int binWidth = 64;
   unsigned int binHeight = 64;
-  own::rectpack::Packer packer(binWidth, binHeight);
+  own::rectpack::Packer packer (binWidth, binHeight);
 
   std::vector<own::rectpack::Rect> rects = {
       {0, 0, 19, 7},
@@ -16,31 +16,29 @@ bool own_rectpack_test() {
       {0, 0, 8, 2},
       {0, 0, 5, 19},
       {0, 0, 18, 11},
-      {0, 0, 14, 13}
-      {0, 0, 21, 4},
-      {0, 0, 14, 13}
-  };
-  std::cout << "Result: \n"
-  for (auto& rect : rects) {
-    std::cout << "Rect " << rect.w << " x " << rect.h << (packer.insert(rect))? "  success" : "  fail" << "\n";
+      {0, 0, 14, 13} {0, 0, 21, 4},
+      {0, 0, 14, 13}};
+  std::cout << "Result: \n" for (auto &rect : rects) {
+    std::cout << "Rect " << rect.w << " x " << rect.h << (packer.insert (rect)) ? "  success" : "  fail"
+            << "\n";
   }
-  
-  char output[binHeight*binWidth];
-  memset(output, ' ', sizeof output);
-  std::cout << "\n\nVisual: \n"
-  char outW = 'A';
-  for (const auto& rect : packer.getPackedRects()) {
-	  for (unsigned y = 0; y < rect.h; ++y) {
-	  	memset(output+((y+rect.y)*binWidth)+rect.x, outW, rect.w);
-	  }
-	  outW++;
+
+  char output[binHeight * binWidth];
+  memset (output, ' ', sizeof output);
+  std::cout << "\n\nVisual: \n" char outW = 'A';
+  for (const auto &rect : packer.getPackedRects ()) {
+    for (unsigned y = 0; y < rect.h; ++y) {
+      memset (output + ((y + rect.y) * binWidth) + rect.x, outW, rect.w);
+    }
+    outW++;
   }
   char outline[binWidth];
   for (unsigned y = 0; y < binHeight; ++y) {
-  	memcpy(outline, output+(y*binWidth), sizeof outline);
-  	std::cout << "\n" << outline;
+    memcpy (outline, output + (y * binWidth), sizeof outline);
+    std::cout << "\n"
+              << outline;
   }
   std::cout << std::endl;
 
-	return true;
+  return true;
 }
