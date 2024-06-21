ac81fc54a158405e7627a2a2b1838a21cdfa655e
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-06-21 12:59:06 +0000

    Core Reformatting Sources

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -2,6 +2,7 @@
 #include "stbi_rectpack.hpp"
 #include "stbi_write.hpp"
 
+#include <algorithm>
 #include <cstdlib>
 #include <cstring>
 #include <filesystem>
@@ -10,7 +11,6 @@
 #include <set>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 namespace fs = std::filesystem;
 
@@ -21,29 +21,27 @@ int main (int argc, char *argv[]) {
     std::cout << "Converting ... " << std::endl;
     if ((argc < 2) || !argv[1] || !argv[1][0])
       throw "Input empty";
-    stbi::rectpack::context p_context(PACKED_SIZE, PACKED_SIZE);
+    stbi::rectpack::context p_context (PACKED_SIZE, PACKED_SIZE);
     std::vector<stbi::rectpack::rect> rects{
-    	{8, 21},
-    	{15, 10},
-    	{30, 20},
-    	{21, 7},
-    	{30, 14}
-  	};
+        {8, 21},
+        {15, 10},
+        {30, 20},
+        {21, 7},
+        {30, 14}};
     std::vector<uint32_t> colors{
-    	0xff0000ff,
-    	0x00ff00ff,
-    	0x0000ffff,
-    	0x00ffffff,
-    	0xffff00ff
-    };
-    
+        0xff0000ff,
+        0x00ff00ff,
+        0x0000ffff,
+        0x00ffffff,
+        0xffff00ff};
+
     if (!stbi::rectpack::pack_rects (&p_context, rects.data (), rects.size ()))
       std::cout << "Warning: All not packed!" << std::endl;
     unsigned int *outBuffer = new unsigned int[PACKED_SIZE * PACKED_SIZE];
     size_t color_count = 0;
     for (stbi::rectpack::rect r : rects) {
       if (!r.was_packed) continue;
-      unsigned int inpBuffer[r.h*r.w];
+      unsigned int inpBuffer[r.h * r.w];
       for (unsigned y = 0; y < r.h; y++)
         std::fill (outBuffer + ((r.y + y) * PACKED_SIZE) + r.x, outBuffer + ((r.y + y) * PACKED_SIZE) + r.x + r.w, colors[color_count]);
       ++color_count;
