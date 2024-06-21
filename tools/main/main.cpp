784740e3976b49e67f22fa0f89f26064f6772339
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-06-21 15:15:14 +0000

    Core Reformatting Sources

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -42,11 +42,11 @@ int main (int argc, char *argv[]) {
     for (stbi::rectpack::rect r : rects) {
       if (!r.was_packed) continue;
       for (size_t y = 0; y < r.h; y++)
-        std::fill_n(outBuffer + ((r.y + y) * PACKED_SIZE) + r.x, r.w, colors[color_count]);
+        std::fill_n (outBuffer + ((r.y + y) * PACKED_SIZE) + r.x, r.w, colors[color_count]);
       ++color_count;
       color_count %= 5;
     }
-    stbi::write::png (argv[1], PACKED_SIZE, PACKED_SIZE, stbi::load::channel::rgb_alpha, (void*)outBuffer, 0);
+    stbi::write::png (argv[1], PACKED_SIZE, PACKED_SIZE, stbi::load::channel::rgb_alpha, (void *)outBuffer, 0);
     delete[] outBuffer;
     std::cout << "Output: " << argv[1] << " completed." << std::endl;
   } catch (const fs::filesystem_error &e) {
