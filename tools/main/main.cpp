7c3a3ed5e14d889a684018bb8e5928c392903d5b
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-06-22 00:03:31 +0800

    fix in 00:03 22/06/24

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -22,22 +22,27 @@ int main (int argc, char *argv[]) {
     if ((argc < 2) || !argv[1] || !argv[1][0])
       throw "Input empty";
     stbi::rectpack::context p_context (PACKED_SIZE, PACKED_SIZE);
+    std::cout << "Contexted ... " << std::endl;
     std::vector<stbi::rectpack::rect> rects{
         {8, 21, 0, 0, 0},
         {15, 10, 0, 0, 0},
         {30, 20, 0, 0, 0},
         {21, 7, 0, 0, 0},
         {30, 14, 0, 0, 0}};
+    std::cout << "Rects ... " << std::endl;
     std::vector<uint32_t> colors{
         0xff0000ff,
         0x00ff00ff,
         0x0000ffff,
         0x00ffffff,
         0xffff00ff};
+    std::cout << "Colors ... " << std::endl;
 
     if (!stbi::rectpack::pack_rects (&p_context, rects.data (), rects.size ()))
       std::cout << "Warning: All not packed!" << std::endl;
+    std::cout << "Packed ... " << std::endl;
     unsigned int *outBuffer = new unsigned int[PACKED_SIZE * PACKED_SIZE];
+    std::cout << "Buffed ... " << std::endl;
     size_t color_count = 0;
     for (stbi::rectpack::rect r : rects) {
       if (!r.was_packed) continue;
@@ -46,8 +51,11 @@ int main (int argc, char *argv[]) {
       ++color_count;
       color_count %= 5;
     }
+    std::cout << "Filled ... " << std::endl;
     stbi::write::png (argv[1], PACKED_SIZE, PACKED_SIZE, stbi::load::channel::rgb_alpha, (void *)outBuffer, 0);
+    std::cout << "Write ... " << std::endl;
     delete[] outBuffer;
+    std::cout << "Cleanup ... " << std::endl;
     std::cout << "Output: " << argv[1] << " completed." << std::endl;
   } catch (const fs::filesystem_error &e) {
     std::cerr << "Error reading directory: " << e.what () << std::endl;
