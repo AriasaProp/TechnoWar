a8b7f70b8177da89f20cc456caf4533bb154ace3
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-06-21 20:58:44 +0800

    fix in 20:58 21/06/24

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -2,7 +2,6 @@
 #include "stbi_rectpack.hpp"
 #include "stbi_write.hpp"
 
-#include <algorithm>
 #include <cstdlib>
 #include <cstring>
 #include <filesystem>
@@ -11,44 +10,46 @@
 #include <set>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 namespace fs = std::filesystem;
 
 #define PACKED_SIZE 1024
-#define NODE_SIZE 5000
 
 int main (int argc, char *argv[]) {
   try {
     std::cout << "Converting ... " << std::endl;
     if ((argc < 2) || !argv[1] || !argv[1][0])
       throw "Input empty";
-    stbi::rectpack::context p_context (PACKED_SIZE, PACKED_SIZE);
+    stbi::rectpack::context p_context(PACKED_SIZE, PACKED_SIZE);
     std::vector<stbi::rectpack::rect> rects{
-        {8, 21},
-        {15, 10},
-        {30, 20},
-        {21, 7},
-        {30, 14}};
+    	{8, 21},
+    	{15, 10},
+    	{30, 20},
+    	{21, 7},
+    	{30, 14}
+  	};
     std::vector<uint32_t> colors{
-        0xff0000ff,
-        0x00ff00ff,
-        0x0000ffff,
-        0x00ffffff,
-        0xffff00ff};
-
+    	0xff0000ff,
+    	0x00ff00ff,
+    	0x0000ffff,
+    	0x00ffffff,
+    	0xffff00ff
+    };
+    
     if (!stbi::rectpack::pack_rects (&p_context, rects.data (), rects.size ()))
       std::cout << "Warning: All not packed!" << std::endl;
     unsigned int *outBuffer = new unsigned int[PACKED_SIZE * PACKED_SIZE];
     size_t color_count = 0;
     for (stbi::rectpack::rect r : rects) {
       if (!r.was_packed) continue;
-      unsigned int inpBuffer[r.h * r.w];
+      unsigned int inpBuffer[r.h*r.w];
       for (unsigned y = 0; y < r.h; y++)
         std::fill (outBuffer + ((r.y + y) * PACKED_SIZE) + r.x, outBuffer + ((r.y + y) * PACKED_SIZE) + r.x + r.w, colors[color_count]);
       ++color_count;
       color_count %= 5;
     }
-    stbi::write::png (argv[2], PACKED_SIZE, PACKED_SIZE, stbi::load::channel::rgb_alpha, outBuffer, 0);
+    stbi::write::png (argv[1], PACKED_SIZE, PACKED_SIZE, stbi::load::channel::rgb_alpha, outBuffer, 0);
     delete[] outBuffer;
     std::cout << "Output: " << argv[2] << " completed." << std::endl;
   } catch (const fs::filesystem_error &e) {
