6e911483fe31a53fc022821c80b750ba3c30e590
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-06-26 18:41:05 +0800

    fix in 18:41 26/06/24

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -50,11 +50,11 @@ int main (int argc, char *argv[]) {
     unsigned int area = 0;
     for (stbi::rectpack::rect &rect : rects) {
       rect.id = 0xff000000 | genRNG (24); // 0 ~ 0x00ffffff
-      rect.w = genRNG (6) + 10;           // (0 ~ 63) + 10
-      rect.h = genRNG (6) + 10;           // (0 ~ 63) + 10
+      rect.w = genRNG (6) + 10;                // (0 ~ 63) + 10
+      rect.h = genRNG (6) + 10;                // (0 ~ 63) + 10
       area += rect.w * rect.h;
     }
-    unsigned int Packed_Size = (unsigned int)(rtInt (area) * 1.2);
+    unsigned int Packed_Size = (unsigned int) (rtInt (area) * 1.2);
     if (!stbi::rectpack::pack_rects (Packed_Size, Packed_Size, rects, RECTS))
       std::cout << "Warning: All not packed! with " << Packed_Size << " px2" << std::endl;
     uint32_t outBuffer[Packed_Size * Packed_Size] = {0};
@@ -70,13 +70,11 @@ int main (int argc, char *argv[]) {
     }
     stbi::write::png (argv[1], Packed_Size, Packed_Size, stbi::load::channel::rgb_alpha, (void *)outBuffer, 0);
     std::cout << "Output: " << argv[1] << " completed." << std::endl;
-    {}
-    catch (const fs::filesystem_error &e) {
-      std::cerr << "Error reading directory: " << e.what () << std::endl;
-      return EXIT_FAILURE;
-    }
-    catch (const char *err) {
-      std::cout << " Error: " << err << std::endl;
-      return EXIT_FAILURE;
-    }
+  } catch (const fs::filesystem_error &e) {
+    std::cerr << "Error reading directory: " << e.what () << std::endl;
+    return EXIT_FAILURE;
+  } catch (const char *err) {
+    std::cout << " Error: " << err << std::endl;
+    return EXIT_FAILURE;
   }
+}
