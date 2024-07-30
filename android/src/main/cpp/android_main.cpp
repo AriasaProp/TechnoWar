aac4260cbdfd6b46b8db14fab452294d631779af
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-07-30 18:17:38 +0000

    Core Reformatting Sources

--- android/src/main/cpp/android_main.cpp
+++ android/src/main/cpp/android_main.cpp
@@ -90,7 +90,7 @@ static void *android_app_entry (void *n) {
     for (;;) {
       switch (ALooper_pollOnce ((started && running) ? 0 : -1, nullptr, nullptr, nullptr)) {
       default:
-      break;
+        break;
       case 1: {
         if (read (app->msgread, &read_cmd, sizeof (msg_pipe)) != sizeof (msg_pipe)) continue;
 #define END_WAITING                    \
@@ -151,55 +151,55 @@ static void *android_app_entry (void *n) {
           break;
         }
 #undef END_WAITING
-      }
-      break;
-      case 0: 
-      if (app->graphics->preRender ()) {
+      } break;
+      case 0:
+        if (app->graphics->preRender ()) {
 
-        a_input.process_event ();
+          a_input.process_event ();
 
-        if (!created) {
-          Main::start ();
-          created = true;
-          resume = false;
-        } else if (resume) {
-          Main::resume ();
-          resume = false;
+          if (!created) {
+            Main::start ();
+            created = true;
+            resume = false;
+          } else if (resume) {
+            Main::resume ();
+            resume = false;
+          }
+          Main::render ();
+          app->graphics->postRender (false);
         }
-        Main::render ();
-        app->graphics->postRender (false);
+        break;
       }
-      break;
     }
-  } catch (...) {
-    // when destroy
-    if (app->graphics->preRender ())
-      Main::end ();
-    created = false;
-    app->graphics->postRender (true);
+    catch (...) {
+      // when destroy
+      if (app->graphics->preRender ())
+        Main::end ();
+      created = false;
+      app->graphics->postRender (true);
+    }
+    // loop ends
+    ALooper_removeFd (looper, app->msgread);
+    delete app->graphics;
+    app->graphics = nullptr;
+    AConfiguration_delete (aconfig);
+    pthread_mutex_lock (&app->mutex);
+    app->destroyed = true;
+    pthread_cond_broadcast (&app->cond);
+    pthread_mutex_unlock (&app->mutex);
+    return NULL;
   }
-  // loop ends
-  ALooper_removeFd (looper, app->msgread);
-  delete app->graphics;
-  app->graphics = nullptr;
-  AConfiguration_delete (aconfig);
-  pthread_mutex_lock (&app->mutex);
-  app->destroyed = true;
-  pthread_cond_broadcast (&app->cond);
-  pthread_mutex_unlock (&app->mutex);
-  return NULL;
-}
 
-static msg_pipe write_cmd;
-void ANativeActivity_onCreate (ANativeActivity *activity, void *, size_t) {
-  // initialize application
-  app = new android_app;
-  pthread_mutex_init (&app->mutex, NULL);
-  pthread_cond_init (&app->cond, NULL);
-  while (pipe (&app->msgread) == -1) {
-    // force loop foor provide pipe
-    LOGE ("Failed to create pipe, %s", strerror (errno));
-  }
+  static msg_pipe write_cmd;
+  void ANativeActivity_onCreate (ANativeActivity * activity, void *, size_t) {
+    // initialize application
+    app = new android_app;
+    pthread_mutex_init (&app->mutex, NULL);
+    pthread_cond_init (&app->cond, NULL);
+    while (pipe (&app->msgread) == -1) {
+      // force loop foor provide pipe
+      LOGE ("Failed to create pipe, %s", strerror (errno));
+    }
 #define WRITE_ANDROID_CMD_W(A, B)                                                   \
   pthread_mutex_lock (&app->mutex);                                                 \
   app->wait_request = true;                                                         \
@@ -219,80 +219,80 @@ void ANativeActivity_onCreate (ANativeActivity *activity, void *, size_t) {
   while (write (app->msgwrite, &write_cmd, sizeof (msg_pipe)) != sizeof (msg_pipe)) \
     LOGE ("cannot write on pipe , %s", strerror (errno));
 
-  // initialize lifecycle
-  activity->callbacks->onStart = [] (ANativeActivity *) {
-    WRITE_ANDROID_CMD (APP_CMD_START, nullptr)
-  };
-  activity->callbacks->onResume = [] (ANativeActivity *) {
-    WRITE_ANDROID_CMD (APP_CMD_RESUME, nullptr)
-  };
-  activity->callbacks->onNativeWindowCreated = [] (ANativeActivity *, ANativeWindow *window) {
-    WRITE_ANDROID_CMD_W (APP_CMD_WINDOW_UPDATE, (void *)window)
-  };
-  activity->callbacks->onInputQueueCreated = [] (ANativeActivity *, AInputQueue *queue) {
-    WRITE_ANDROID_CMD_W (APP_CMD_INPUT_UPDATE, (void *)queue)
-  };
-  activity->callbacks->onConfigurationChanged = [] (ANativeActivity *) {
-    WRITE_ANDROID_CMD (APP_CMD_CONFIG_CHANGED, nullptr)
-  };
-  activity->callbacks->onLowMemory = [] (ANativeActivity *) {
-    WRITE_ANDROID_CMD (APP_CMD_LOW_MEMORY, nullptr)
-  };
-  activity->callbacks->onWindowFocusChanged = [] (ANativeActivity *, int focused) {
-    WRITE_ANDROID_CMD_W (APP_CMD_FOCUS_CHANGED, reinterpret_cast<void *> (focused));
-  };
-  activity->callbacks->onNativeWindowResized = [] (ANativeActivity *, ANativeWindow *) {
-    WRITE_ANDROID_CMD (APP_CMD_WINDOW_RESIZED, nullptr)
-  };
-  activity->callbacks->onContentRectChanged = [] (ANativeActivity *, const ARect *) {
-    WRITE_ANDROID_CMD (APP_CMD_CONTENT_RECT_CHANGED, nullptr)
-  };
-  activity->callbacks->onSaveInstanceState = [] (ANativeActivity *, size_t *outLen) -> void * {
-    WRITE_ANDROID_CMD (APP_CMD_SAVE_STATE, nullptr)
-    *outLen = 0;
-    return nullptr;
-  };
-  activity->callbacks->onNativeWindowDestroyed = [] (ANativeActivity *, ANativeWindow *) {
-    WRITE_ANDROID_CMD_W (APP_CMD_WINDOW_UPDATE, nullptr)
-  };
-  activity->callbacks->onInputQueueDestroyed = [] (ANativeActivity *, AInputQueue *) {
-    WRITE_ANDROID_CMD_W (APP_CMD_INPUT_UPDATE, nullptr)
-  };
-  activity->callbacks->onPause = [] (ANativeActivity *) {
-    WRITE_ANDROID_CMD_W (APP_CMD_PAUSE, nullptr)
-  };
-  activity->callbacks->onStop = [] (ANativeActivity *) {
-    WRITE_ANDROID_CMD (APP_CMD_STOP, nullptr)
-  };
-  activity->callbacks->onDestroy = [] (ANativeActivity *) {
-    WRITE_ANDROID_CMD (APP_CMD_DESTROY, nullptr)
-    pthread_mutex_lock (&app->mutex);
-    while (!app->destroyed)
-      pthread_cond_wait (&app->cond, &app->mutex);
-    pthread_mutex_unlock (&app->mutex);
-    close (app->msgread);
-    close (app->msgwrite);
-    pthread_cond_destroy (&app->cond);
-    pthread_mutex_destroy (&app->mutex);
-    delete app;
-  };
+    // initialize lifecycle
+    activity->callbacks->onStart = [] (ANativeActivity *) {
+      WRITE_ANDROID_CMD (APP_CMD_START, nullptr)
+    };
+    activity->callbacks->onResume = [] (ANativeActivity *) {
+      WRITE_ANDROID_CMD (APP_CMD_RESUME, nullptr)
+    };
+    activity->callbacks->onNativeWindowCreated = [] (ANativeActivity *, ANativeWindow *window) {
+      WRITE_ANDROID_CMD_W (APP_CMD_WINDOW_UPDATE, (void *)window)
+    };
+    activity->callbacks->onInputQueueCreated = [] (ANativeActivity *, AInputQueue *queue) {
+      WRITE_ANDROID_CMD_W (APP_CMD_INPUT_UPDATE, (void *)queue)
+    };
+    activity->callbacks->onConfigurationChanged = [] (ANativeActivity *) {
+      WRITE_ANDROID_CMD (APP_CMD_CONFIG_CHANGED, nullptr)
+    };
+    activity->callbacks->onLowMemory = [] (ANativeActivity *) {
+      WRITE_ANDROID_CMD (APP_CMD_LOW_MEMORY, nullptr)
+    };
+    activity->callbacks->onWindowFocusChanged = [] (ANativeActivity *, int focused) {
+      WRITE_ANDROID_CMD_W (APP_CMD_FOCUS_CHANGED, reinterpret_cast<void *> (focused));
+    };
+    activity->callbacks->onNativeWindowResized = [] (ANativeActivity *, ANativeWindow *) {
+      WRITE_ANDROID_CMD (APP_CMD_WINDOW_RESIZED, nullptr)
+    };
+    activity->callbacks->onContentRectChanged = [] (ANativeActivity *, const ARect *) {
+      WRITE_ANDROID_CMD (APP_CMD_CONTENT_RECT_CHANGED, nullptr)
+    };
+    activity->callbacks->onSaveInstanceState = [] (ANativeActivity *, size_t *outLen) -> void * {
+      WRITE_ANDROID_CMD (APP_CMD_SAVE_STATE, nullptr)
+      *outLen = 0;
+      return nullptr;
+    };
+    activity->callbacks->onNativeWindowDestroyed = [] (ANativeActivity *, ANativeWindow *) {
+      WRITE_ANDROID_CMD_W (APP_CMD_WINDOW_UPDATE, nullptr)
+    };
+    activity->callbacks->onInputQueueDestroyed = [] (ANativeActivity *, AInputQueue *) {
+      WRITE_ANDROID_CMD_W (APP_CMD_INPUT_UPDATE, nullptr)
+    };
+    activity->callbacks->onPause = [] (ANativeActivity *) {
+      WRITE_ANDROID_CMD_W (APP_CMD_PAUSE, nullptr)
+    };
+    activity->callbacks->onStop = [] (ANativeActivity *) {
+      WRITE_ANDROID_CMD (APP_CMD_STOP, nullptr)
+    };
+    activity->callbacks->onDestroy = [] (ANativeActivity *) {
+      WRITE_ANDROID_CMD (APP_CMD_DESTROY, nullptr)
+      pthread_mutex_lock (&app->mutex);
+      while (!app->destroyed)
+        pthread_cond_wait (&app->cond, &app->mutex);
+      pthread_mutex_unlock (&app->mutex);
+      close (app->msgread);
+      close (app->msgwrite);
+      pthread_cond_destroy (&app->cond);
+      pthread_mutex_destroy (&app->mutex);
+      delete app;
+    };
 #undef WRITE_ANDROID_CMD_W
 #undef WRITE_ANDROID_CMD
 
-  // start thread game
-  pthread_attr_t attr;
-  pthread_attr_init (&attr);
-  pthread_attr_setdetachstate (&attr, PTHREAD_CREATE_DETACHED);
-  pthread_create (&app->thread, &attr, android_app_entry, activity);
-  pthread_attr_destroy (&attr);
-}
+    // start thread game
+    pthread_attr_t attr;
+    pthread_attr_init (&attr);
+    pthread_attr_setdetachstate (&attr, PTHREAD_CREATE_DETACHED);
+    pthread_create (&app->thread, &attr, android_app_entry, activity);
+    pthread_attr_destroy (&attr);
+  }
 
-// native MainActivity.java
+  // native MainActivity.java
 
-extern "C" JNIEXPORT void Java_com_ariasaproject_technowar_MainActivity_insetNative (JNIEnv *, jobject, jint left, jint top, jint right, jint bottom) {
-  if (!app->graphics) return;
-  app->graphics->cur_safe_insets[0] = left;
-  app->graphics->cur_safe_insets[1] = top;
-  app->graphics->cur_safe_insets[2] = right;
-  app->graphics->cur_safe_insets[3] = bottom;
-}
+  extern "C" JNIEXPORT void Java_com_ariasaproject_technowar_MainActivity_insetNative (JNIEnv *, jobject, jint left, jint top, jint right, jint bottom) {
+    if (!app->graphics) return;
+    app->graphics->cur_safe_insets[0] = left;
+    app->graphics->cur_safe_insets[1] = top;
+    app->graphics->cur_safe_insets[2] = right;
+    app->graphics->cur_safe_insets[3] = bottom;
+  }
