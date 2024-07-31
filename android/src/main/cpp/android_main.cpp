7ee42c5f956b6ce7507cad22862a6b7b4e5e9464
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-07-31 06:31:07 +0000

    Core Reformatting Sources

--- android/src/main/cpp/android_main.cpp
+++ android/src/main/cpp/android_main.cpp
@@ -87,14 +87,14 @@ static void *android_app_entry (void *n) {
     msg_pipe read_cmd{
         APP_CMD_CREATE,
         nullptr};
-  	int poll_result;
+    int poll_result;
     for (;;) {
-    	do {
-    		poll_result = ALooper_pollOnce ((started && running) ? 0 : -1, nullptr, nullptr, nullptr);
-    	} while (poll_result == ALOOPER_POLL_CALLBACK);
+      do {
+        poll_result = ALooper_pollOnce ((started && running) ? 0 : -1, nullptr, nullptr, nullptr);
+      } while (poll_result == ALOOPER_POLL_CALLBACK);
       switch (poll_result) {
       case 1:
-      	//activity handler
+        // activity handler
         if (read (app->msgread, &read_cmd, sizeof (msg_pipe)) == sizeof (msg_pipe)) {
 #define END_WAITING                    \
   pthread_mutex_lock (&app->mutex);    \
@@ -157,7 +157,7 @@ static void *android_app_entry (void *n) {
         }
         break;
       case 0:
-      	//base render
+        // base render
         if (app->graphics->preRender ()) {
           a_input.process_event ();
 
