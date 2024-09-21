2ee407aeafa242fb6ac60f9685815cd6dda64b5f
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-09-21 21:22:26 +0800

    file-ing : 21:22:26 09/21/24

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -4,11 +4,23 @@
 
 namespace fs = std::filesystem;
 
+//test group
+extern void stbi_rectpack_test ();
+
 //converting group
 extern void assets_for_android(fs::path, fs::path);
 extern void assets_for_desktop(fs::path, fs::path);
 
 int main (int argc, char *argv[]) {
+	std::cout << "Test Tools";
+  try {
+  	stbi_rectpack_test ();
+  } catch (const char *err) {
+  	std::cout << "\nError -> " << err << std::endl;
+  	return EXIT_FAILURE;
+  }
+	std::cout << " Done!" << std::endl;
+	
   std::cout << "Converting Assets";
   try {
   	try {
