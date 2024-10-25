a512b7b3002c0750f636ada671427fd517709c35
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-10-25 18:03:39 +0800

    tools-test : 18:03:39 10/25/24

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -12,16 +12,16 @@ extern void assets_for_android (fs::path, fs::path);
 extern void assets_for_desktop (fs::path, fs::path);
 
 int main (int argc, char *argv[]) {
-  std::cout << "Test Tools ";
+  std::cerr << "Test Tools ";
   try {
     stbi_rectpack_test ();
   } catch (const char *err) {
-    std::cout << "Error!\n " << err << std::endl;
+    std::cerr << "Error!\n " << err << std::endl;
     return EXIT_FAILURE;
   }
-  std::cout << "Done!" << std::endl;
+  std::cerr << "Done!" << std::endl;
 
-  std::cout << "Converting Assets ";
+  std::cerr << "Converting Assets ";
   try {
     // path of source files
     fs::path rootDir = argv[1];
@@ -30,18 +30,18 @@ int main (int argc, char *argv[]) {
     if (!fs::exists (assets) || !fs::is_directory (assets)) throw "assets folder didn't exist!";
 
     assets_for_android (rootDir / "android/src/main/assets", assets);
-    // assets_for_desktop (rootDir / "desktop/assets", assets);
+    //assets_for_desktop (rootDir / "desktop/assets", assets);
 
   } catch (const fs::filesystem_error &e) {
-    std::cout << "Error(Filesystem): " << e.what () << std::endl;
+    std::cerr << "Error(Filesystem): " << e.what () << std::endl;
     return EXIT_FAILURE;
   } catch (const char *err) {
-    std::cout << "Error: " << err << std::endl;
+    std::cerr << "Error: " << err << std::endl;
     return EXIT_FAILURE;
   } catch (...) {
-    std::cout << "Error: Unknown" << std::endl;
+    std::cerr << "Error: Unknown" << std::endl;
     return EXIT_FAILURE;
   }
-  std::cout << "Done!" << std::endl;
+  std::cerr << "Done!" << std::endl;
   return EXIT_SUCCESS;
 }
