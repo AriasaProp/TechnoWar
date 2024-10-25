29b285a7b802b4ec6326e0946d9a79e20c040353
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-10-25 13:52:16 +0800

    up : 13:52:16 10/25/24

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -21,27 +21,27 @@ int main (int argc, char *argv[]) {
   }
   std::cout << "Done!" << std::endl;
 
-  std::cout << "Converting Assets";
+  std::cout << "Converting Assets ";
   try {
-    try {
-      // path of source files
-      fs::path rootDir = argv[1];
-      fs::path projectDir = argv[2];
-      fs::path assets = projectDir / "assets";
-      if (!fs::exists (assets) || !fs::is_directory (assets)) throw "assets folder didn't exist!";
+    // path of source files
+    fs::path rootDir = argv[1];
+    fs::path projectDir = argv[2];
+    fs::path assets = projectDir / "assets";
+    if (!fs::exists (assets) || !fs::is_directory (assets)) throw "assets folder didn't exist!";
 
-      assets_for_android (rootDir / "android/src/main/assets", assets);
-      assets_for_desktop (rootDir / "desktop/assets", assets);
+    assets_for_android (rootDir / "android/src/main/assets", assets);
+    assets_for_desktop (rootDir / "desktop/assets", assets);
 
-    } catch (const fs::filesystem_error &e) {
-      std::string thr_er = "(Filesystem) ";
-      thr_er += e.what ();
-      throw thr_er.c_str ();
-    }
+  } catch (const fs::filesystem_error &e) {
+    std::cout << "Error(Filesystem): " << e.what () << std::endl;
+    return EXIT_FAILURE;
   } catch (const char *err) {
-    std::cout << " Error:" << err << std::endl;
+    std::cout << "Error: " << err << std::endl;
+    return EXIT_FAILURE;
+  } catch (...) {
+    std::cout << "Error: Unknown" << std::endl;
     return EXIT_FAILURE;
   }
-  std::cout << " Done!" << std::endl;
+  std::cout << "Done!" << std::endl;
   return EXIT_SUCCESS;
 }
