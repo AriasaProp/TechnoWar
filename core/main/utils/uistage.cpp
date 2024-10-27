365837fe14425d02c18db4a4252aa47334fab6a6
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-10-27 04:00:04 +0000

    Reformatting Code Sources

--- core/main/utils/uistage.cpp
+++ core/main/utils/uistage.cpp
@@ -164,11 +164,11 @@ void uistage::draw (float delta) {
       xList[0] = x - 5.0f;                                               // minx
       xList[1] = x + tlp.width + 5.0f;                                   // maxx
       yList[0] = y - (static_cast<float> (font->LineHeight) * F) - 5.0f; // miny
-      yList[1] = y + 5.0f;                                              // maxy
+      yList[1] = y + 5.0f;                                               // maxy
       if (tlp.lifetime < 1.65f)
-        clr.rgba.a = (uint8_t) round(136.0f * tlp.lifetime / 1.65f);
+        clr.rgba.a = (uint8_t)round (136.0f * tlp.lifetime / 1.65f);
       else
-      	clr.rgba.a = 0x88;
+        clr.rgba.a = 0x88;
       *(verts++) = {xList[0], yList[0], clr, 0, 0};
       *(verts++) = {xList[0], yList[1], clr, 0, 0};
       *(verts++) = {xList[1], yList[0], clr, 0, 0};
@@ -185,9 +185,9 @@ void uistage::draw (float delta) {
     for (size_t i = 0; i < 7; ++i) {
       tooltip &tlp = tooltips[i];
       if (tlp.lifetime <= 0.0f) break;
-    	clr = font->fcolor;
+      clr = font->fcolor;
       if (tlp.lifetime < 1.65f)
-        clr.rgba.a = (uint8_t)round(255.0f * tlp.lifetime / 1.65f);
+        clr.rgba.a = (uint8_t)round (255.0f * tlp.lifetime / 1.65f);
       float x = (engine::graphics::getWidth () - tlp.width) * .5f;
       float y = engine::graphics::getHeight () * 0.75f + ((static_cast<float> (font->LineHeight) * font->fscale ()) + 10.5f) * i + 10.5f;
 
