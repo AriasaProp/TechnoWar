16e7384693913ad2514d1a80d2af988f4c200363
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-10-30 09:32:47 +0800

    back-to-png : 09:32:47 10/30/24

--- tools/main/image_rewrite.cpp
+++ tools/main/image_rewrite.cpp
@@ -26,8 +26,6 @@ void image_rewrite (fs::path assets, fs::path converted) {
   if (fs::exists (image_result_path) || !fs::create_directory (image_result_path))
     throw "error make converted dir";
   // find all subfolder inside image
-  unsigned char *img_src;
-  int dih[3];
   for (const fs::directory_entry &image : fs::directory_iterator (image_path)) {
     if (!fs::is_regular_file (image.status ())) continue;
     std::string image_path = image.path ().string ();
@@ -36,22 +34,24 @@ void image_rewrite (fs::path assets, fs::path converted) {
             image_path.ends_with (".jpeg") ||
             image_path.ends_with (".jpg") ||
             image_path.ends_with (".png"))) continue;
+  /*
+  int dih[3];
+  unsigned char *img_src;
     img_src = stbi::load::load_from_filename (image_path.c_str (), dih, dih + 1, dih + 2, stbi::load::channel::rgb_alpha);
     if (!img_src) throw stbi::load::failure_reason ();
-    /*
-      {
-        size_t lastSlashPos = image_path.find_last_of ("/\\");
-        lastSlashPos = (lastSlashPos == std::string::npos) ? 0 : lastSlashPos + 1;
-        size_t lastDotPos = image_path.find_last_of ('.');
-        if (lastDotPos == std::string::npos || lastDotPos < lastSlashPos) {
-          lastDotPos = image_path.length ();
-        }
-        image_path = image_path.substr (lastSlashPos, lastDotPos - lastSlashPos);
-        fs::path res = image_result_path / image_path;
-        res += ".png";
-        stbi::write::png (res.c_str (), dih[0], dih[1], 4, img_src, 4);
+    {
+      size_t lastSlashPos = image_path.find_last_of ("/\\");
+      lastSlashPos = (lastSlashPos == std::string::npos) ? 0 : lastSlashPos + 1;
+      size_t lastDotPos = image_path.find_last_of ('.');
+      if (lastDotPos == std::string::npos || lastDotPos < lastSlashPos) {
+        lastDotPos = image_path.length ();
       }
-    */
+      image_path = image_path.substr (lastSlashPos, lastDotPos - lastSlashPos);
+      fs::path res = image_result_path / image_path;
+      res += ".png";
+      stbi::write::png (res.c_str (), dih[0], dih[1], 4, img_src, 4);
+    }
+  */
     stbi::load::image_free (img_src);
   }
 }
