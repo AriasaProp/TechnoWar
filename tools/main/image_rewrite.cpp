88e7f38ababdca48b21e9a6406253b35466f0f9c
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-10-30 09:28:30 +0800

    back-to-png : 09:28:30 10/30/24

--- tools/main/image_rewrite.cpp
+++ tools/main/image_rewrite.cpp
@@ -26,7 +26,6 @@ void image_rewrite (fs::path assets, fs::path converted) {
   if (fs::exists (image_result_path) || !fs::create_directory (image_result_path))
     throw "error make converted dir";
   // find all subfolder inside image
-  /*
   unsigned char *img_src;
   int dih[3];
   for (const fs::directory_entry &image : fs::directory_iterator (image_path)) {
@@ -39,6 +38,7 @@ void image_rewrite (fs::path assets, fs::path converted) {
             image_path.ends_with (".png"))) continue;
     img_src = stbi::load::load_from_filename (image_path.c_str (), dih, dih + 1, dih + 2, stbi::load::channel::rgb_alpha);
     if (!img_src) throw stbi::load::failure_reason ();
+  /*
     {
       size_t lastSlashPos = image_path.find_last_of ("/\\");
       lastSlashPos = (lastSlashPos == std::string::npos) ? 0 : lastSlashPos + 1;
@@ -51,7 +51,7 @@ void image_rewrite (fs::path assets, fs::path converted) {
       res += ".png";
       stbi::write::png (res.c_str (), dih[0], dih[1], 4, img_src, 4);
     }
+  */
     stbi::load::image_free (img_src);
   }
-  */
 }
