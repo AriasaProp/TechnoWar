d953a6ab8431150d468f47ae37fddd07175c0f2e
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-11-01 09:28:39 +0800

    try-using-uistage : 09:28:39 11/01/24

--- core/main/utils/uistage.cpp
+++ core/main/utils/uistage.cpp
@@ -91,7 +91,7 @@ void uistage::loadUISkin (const char *uiSkin) {
   }
 
   {
-    sprintf (temp_char_buffer, "%s.png", uiSkin);
+    sprintf (temp_char_buffer, "%s.png", "images/test");
     int x, y, z;
     unsigned char *tex_px = stbi::load::load_from_assets (temp_char_buffer, &x, &y, &z, stbi::load::channel::rgb_alpha);
     uiskin::tex = engine::graphics::gen_texture (x, y, tex_px);
