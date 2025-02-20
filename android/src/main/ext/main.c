30d5c2c75293db3a1ab7db32c57527484d83de92
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-02-20 00:47:16 +0000

    Reformatting Code Sources

--- android/src/main/ext/main.c
+++ android/src/main/ext/main.c
@@ -7,8 +7,6 @@
 #include <android/native_activity.h>
 #include <android/sensor.h>
 #include <android/set_abort_message.h>
-#include <android/configuration.h>
-#include <android/looper.h>
 
 #include <assert.h>
 #include <errno.h>
