d8b6e2548b3d863ae67e74ee0fa5543e17f8d45d
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-02-21 06:45:35 +0000

    Reformatting Code Sources

--- core/main/include/core.h
+++ core/main/include/core.h
@@ -1,6 +1,6 @@
 struct core {
-	float x, y;
+  float x, y;
 };
 
 extern void Main_update (struct core *);
-extern void Main_term(struct core *);
+extern void Main_term (struct core *);
--- core/main/src/core.c
+++ core/main/src/core.c
@@ -1,6 +1,6 @@
+#include "core.h"
 #include "engine.h"
 #include "util.h"
-#include "core.h"
 
 void Main_update (struct core *ic) {
   if ((ic->x += 0.08f) > 1.0f) ic->x = 0.f;
@@ -8,14 +8,13 @@ void Main_update (struct core *ic) {
 
   // Just fill the screen with a color.
   get_engine ()->g.clearColor ((struct fcolor){
-  	((float)get_engine ()->i.getTouch(0).x / engine->width * 0.5f) + (ic->x * 0.5f),
-  	((float)get_engine ()->i.getTouch(0).y * 0.5f) + (ic->y * 0.5f),
-  	((float)get_engine ()->i.getTouch(0).y / engine->height * 0.3f) + (ic->x * 0.3f) + (ic->y * 0.4f),
-  	1.0f
-  });
+      ((float)get_engine ()->i.getTouch (0).x / engine->width * 0.5f) + (ic->x * 0.5f),
+      ((float)get_engine ()->i.getTouch (0).y * 0.5f) + (ic->y * 0.5f),
+      ((float)get_engine ()->i.getTouch (0).y / engine->height * 0.3f) + (ic->x * 0.3f) + (ic->y * 0.4f),
+      1.0f});
   get_engine ()->g.clear (GL_COLOR_BUFFER_BIT);
 }
 
 void Main_term (struct core *ic) {
-	free_mem (ic);
+  free_mem (ic);
 }
