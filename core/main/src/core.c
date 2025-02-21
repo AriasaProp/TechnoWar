8cc3dde9b9ac94769da2db585d618030659aa254
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-02-21 15:45:41 +0800

    f4 : 15:45:41 02/21/25

--- core/main/src/core.c
+++ core/main/src/core.c
@@ -1,6 +1,6 @@
-#include "core.h"
 #include "engine.h"
 #include "util.h"
+#include "core.h"
 
 void Main_update (struct core *ic) {
   if ((ic->x += 0.08f) > 1.0f) ic->x = 0.f;
@@ -8,13 +8,14 @@ void Main_update (struct core *ic) {
 
   // Just fill the screen with a color.
   get_engine ()->g.clearColor ((struct fcolor){
-      ((float)get_engine ()->i.getTouch (0).x / get_engine ()->g.getScreenSize.x * 0.5f) + (ic->x * 0.5f),
-      ((float)get_engine ()->i.getTouch (0).y * 0.5f) + (ic->y * 0.5f),
-      ((float)get_engine ()->i.getTouch (0).y / get_engine ()->g.getScreenSize.y * 0.3f) + (ic->x * 0.3f) + (ic->y * 0.4f),
-      1.0f});
+  	((float)get_engine ()->i.getTouch(0).x / get_engine ()->g.getScreenSize().x * 0.5f) + (ic->x * 0.5f),
+  	((float)get_engine ()->i.getTouch(0).y * 0.5f) + (ic->y * 0.5f),
+  	((float)get_engine ()->i.getTouch(0).y / get_engine ()->g.getScreenSize().y * 0.3f) + (ic->x * 0.3f) + (ic->y * 0.4f),
+  	1.0f
+  });
   get_engine ()->g.clear (GRAPHICS_CLEAR_COLOR);
 }
 
 void Main_term (struct core *ic) {
-  free_mem (ic);
+	free_mem (ic);
 }
