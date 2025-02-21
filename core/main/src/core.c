7e25bd805bb6728be283aa8a4eabb84f3e4a630a
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-02-21 22:15:06 +0800

    f5. : 22:15:06 02/21/25

--- core/main/src/core.c
+++ core/main/src/core.c
@@ -2,18 +2,21 @@
 #include "engine.h"
 #include "util.h"
 
-struct flat_vertex triangle[3]{
-    {(struct vec2){200.0f, 200.0f}, (struct vec2){0.0f, 0.0f}},
-    {(struct vec2){700.0f, 200.0f}, (struct vec2){0.0f, 0.0f}},
-    {(struct vec2){300.0f, 700.0f}, (struct vec2){0.0f, 0.0f}}};
+struct flat_vertex triangle[] = {
+	{(struct vec2) {200.0f, 200.0f}, (struct vec2) {0.0f, 0.0f} }, 
+	{(struct vec2) {700.0f, 200.0f}, (struct vec2) {0.0f, 0.0f} }, 
+	{(struct vec2) {300.0f, 700.0f}, (struct vec2) {0.0f, 0.0f} } 
+};
 
-struct core core_cache = {0};
+struct core core_cache = { 0 };
+
+
 
 void Main_update (struct core *ic) {
   if ((ic->x += 0.08f) > 1.0f) ic->x = 0.f;
   if ((ic->y += 0.15f) > 1.0f) ic->y = 0.f;
-
-  get_engine ()->g.flatRender (0, triangle, 3);
+  
+  get_engine()->g.flatRender(0, triangle, 3);
 }
 
 void Main_term (struct core *ic) {
