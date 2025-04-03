a4d760f971db3edd692d99dcab3cec9e0007ad67
Ariasa Prop <ikomangwidiadaariasa12@gmail.com>
2025-04-03 23:17:13 +0800

    addSDL

--- android/src/main/ext/assetManager.c
+++ android/src/main/ext/assetManager.c
@@ -6,11 +6,10 @@ static AAssetManager *mngr = NULL;
 
 static AAsset *reading[MAX_ASSET_READING] = { 0 };
 
-static int assetBuffer(const char *filename, void **buf, int *len)
-{
+static int assetBuffer(const char *filename, void **buf, int *len) {
   for (size_t i = 0; i < MAX_ASSET_READING; ++i) {
     if (!reading[i]) {
-      reading[i] = AAssetManager_open(assetManager, filename, AASSET_MODE_BUFFER);
+      reading[i] = AAssetManager_open(mngr, filename, AASSET_MODE_BUFFER);
       *len = AAsset_getLength(reading[i]);
       *buf = AAsset_getBuffer(reading[i]);
       return i;
@@ -19,32 +18,27 @@ static int assetBuffer(const char *filename, void **buf, int *len)
   return -1;
 }
 
-static int openAsset(const char *filename)
-{
+static int openAsset(const char *filename) {
   for (size_t i = 0; i < MAX_ASSET_READING; ++i) {
     if (!reading[i]) {
-      reading[i] = AAssetManager_open(assetManager, filename, AASSET_MODE_STREAMING);
+      reading[i] = AAssetManager_open(mngr, filename, AASSET_MODE_STREAMING);
       return i;
     }
   }
   return -1;
 }
-static int assetRead(int a, void *buf, size_t count)
-{
+static int assetRead(int a, void *buf, size_t count) {
   return AAsset_read(reading[a], buf, count);
 }
-static size_t assetLength(int a)
-{
+static size_t assetLength(int a) {
   return AAsset_getRemainingLength(reading[a]);
 }
-static void assetClose(int a)
-{
+static void assetClose(int a) {
   AAsset_close(reading[a]);
   reading[a] = NULL;
 }
 
-void android_assetManager_init(AAssetManager *m)
-{
+void android_assetManager_init(AAssetManager *m) {
   mngr = m;
 
   get_engine()->a.assetBuffer = assetBuffer;
@@ -53,8 +47,7 @@ void android_assetManager_init(AAssetManager *m)
   get_engine()->a.assetLength = assetLength;
   get_engine()->a.assetClose = assetClose;
 }
-void android_assetManager_term()
-{
+void android_assetManager_term() {
   mngr = NULL;
 
   get_engine()->a.assetBuffer = NULL;
