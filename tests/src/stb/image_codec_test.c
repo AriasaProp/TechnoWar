5d5ea9384a34fb0f93ee265291d331703834afc1
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-07-11 09:20:51 +0800

    try old : 09:20:51 07/11/25

--- tests/src/stb/image_codec_test.c
+++ tests/src/stb/image_codec_test.c
@@ -9,17 +9,18 @@
 #define TEST_COUNT 6
 
 int image_codec_test() {
+  #define ERROR_MSG_LENGTH 512
 	printf("  Image Codec:\n");
 	void *timer = start_timing();
 	char buffer[256];
-	char errorMsg[256] = {0};
+	char errorMsg[ERROR_MSG_LENGTH] = {0};
 	int temp[4];
 	for (int i = 0; (i < TEST_COUNT) && (!*errorMsg); ++i) {
 		stbi_clean_failure();
 		sprintf(buffer, "tests/data/%02d.png", i);
 		FILE *imf = fopen(buffer, "rb");
 		if (!imf) {
-			sprintf(errorMsg, "failed load I/O file %s", buffer);
+			snprintf(errorMsg, ERROR_MSG_LENGTH, "failed load I/O file %s", buffer);
 			continue;
 		}
 		fseek(imf, 0, SEEK_END);
@@ -32,7 +33,7 @@ int image_codec_test() {
 			fclose(imf);
 		} else {
 			fclose(imf);
-			sprintf(errorMsg, "failed allocate memory for image %s", buffer);
+			snprintf(errorMsg, ERROR_MSG_LENGTH,  "failed allocate memory for image %s", buffer);
 			continue;
 		}
 		unsigned char *load = stbi_load_from_memory(im_mem, mem_len, temp, temp + 1, temp + 2, 0);
@@ -40,19 +41,19 @@ int image_codec_test() {
 			unsigned char *encode = stbi_write_png_to_mem(load, temp[0], temp[1], temp[2], temp + 3);
 			if (encode) {
 				if (temp[3] != mem_len) {
-					sprintf(errorMsg, "failed result len %d != %ld %s", temp[3], mem_len, buffer);
+					snprintf(errorMsg, ERROR_MSG_LENGTH,  "failed result len %d != %ld %s", temp[3], mem_len, buffer);
 				} else if (memcmp(encode, im_mem, mem_len)) {
-					sprintf(errorMsg, "failed different result %s", buffer);
+					snprintf(errorMsg, ERROR_MSG_LENGTH,  "failed different result %s", buffer);
 				}
 				free (encode);
 			} else {
-			  sprintf(errorMsg, "failed write %s", buffer);
+			  snprintf(errorMsg, ERROR_MSG_LENGTH,  "failed write %s", buffer);
 			}
 			free (load);
 			free(im_mem);
 		} else {
 			free(im_mem);
-			sprintf(errorMsg, "failed load %s cause %s", buffer, stbi_get_failure_reason());
+			snprintf(errorMsg, ERROR_MSG_LENGTH,  "failed load %s cause %s", buffer, stbi_get_failure_reason());
 		}
 	}
   if (*errorMsg) printf(RED "   Failure: %s" RESET, errorMsg);
--- tests/src/stb/rect_pack_test.c
+++ tests/src/stb/rect_pack_test.c
@@ -10,14 +10,15 @@
 #include <timing.h>
 
 int rect_pack_test (void) {
+  #define ERROR_MSG_LENGTH 512
 	printf("  Rect Pack:\n");
-	char errorMsg[512] = {0};
+	char errorMsg[ERROR_MSG_LENGTH] = {0};
 	void *timer = start_timing();
 	srand(time(0));
 	int total_rect = (rand() % 21) + 15;
 	struct stbrp_rect *rects = (struct stbrp_rect* )malloc(sizeof(struct stbrp_rect ) * total_rect);
 	if (!rects) {
-		sprintf(errorMsg, "memory allocation failed");
+		snprintf(errorMsg, ERROR_MSG_LENGTH,  "memory allocation failed");
 		goto rect_pack_test_end;
 	}
 	size_t total_area = 0;
@@ -29,7 +30,7 @@ int rect_pack_test (void) {
 	}
 	total_area = (size_t) (sqrt(total_area) * 1.3);
 	if (!stbrp_pack_rects(rects, total_rect, total_area, total_area)) {
-		sprintf(errorMsg, "rect is not fit");
+		snprintf(errorMsg, ERROR_MSG_LENGTH,  "rect is not fit");
 		printf("   Rect %zu^2: \n", total_area);
 		for (int i = 0; i < total_rect; ++i)
 			printf("    %d: {%d, %d, %d, %d}\n", i, rects[i].x, rects[i].y, rects[i].w, rects[i].h);
