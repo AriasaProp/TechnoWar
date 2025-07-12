a42615dc13f8f69d2cdd80c12043d075a3d2bc3a
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-07-12 00:32:46 +0000

    Reformatting Code Sources

--- android/src/main/java/com/ariasaproject/technowar/MainActivity.java
+++ android/src/main/java/com/ariasaproject/technowar/MainActivity.java
@@ -12,49 +12,49 @@ public class MainActivity extends NativeActivity /* implements View.OnApplyWindo
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    //getWindow().getDecorView().setOnApplyWindowInsetsListener(this);
+    // getWindow().getDecorView().setOnApplyWindowInsetsListener(this);
   }
-/*
-  @Override
-  public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
-    int insetsL = 0, insetsT = 0, insetsR = 0, insetsB = 0;
-    try {
-      if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) && (insets != null)) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-          int tl =
-            insets.getRoundedCorner(android.view.RoundedCorner.POSITION_TOP_LEFT)
-              .getRadius();
-          int bl =
-            insets.getRoundedCorner(android.view.RoundedCorner.POSITION_BOTTOM_LEFT)
-              .getRadius();
-          insetsL = Math.max(tl, bl) / 3;
-          int tr =
-            insets.getRoundedCorner(android.view.RoundedCorner.POSITION_TOP_RIGHT)
-              .getRadius();
-          insetsT = Math.max(tl, tr) / 3;
-          int br =
-            insets.getRoundedCorner(
-                    android.view.RoundedCorner.POSITION_BOTTOM_RIGHT)
-              .getRadius();
-          insetsR = Math.max(tr, br) / 3;
-          insetsB = Math.max(bl, br) / 3;
+  /*
+    @Override
+    public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+      int insetsL = 0, insetsT = 0, insetsR = 0, insetsB = 0;
+      try {
+        if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) && (insets != null)) {
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            int tl =
+              insets.getRoundedCorner(android.view.RoundedCorner.POSITION_TOP_LEFT)
+                .getRadius();
+            int bl =
+              insets.getRoundedCorner(android.view.RoundedCorner.POSITION_BOTTOM_LEFT)
+                .getRadius();
+            insetsL = Math.max(tl, bl) / 3;
+            int tr =
+              insets.getRoundedCorner(android.view.RoundedCorner.POSITION_TOP_RIGHT)
+                .getRadius();
+            insetsT = Math.max(tl, tr) / 3;
+            int br =
+              insets.getRoundedCorner(
+                      android.view.RoundedCorner.POSITION_BOTTOM_RIGHT)
+                .getRadius();
+            insetsR = Math.max(tr, br) / 3;
+            insetsB = Math.max(bl, br) / 3;
+          }
+          final android.view.DisplayCutout displayCutout = insets.getDisplayCutout();
+          if (displayCutout != null) {
+            insetsL = Math.max(insetsL, displayCutout.getSafeInsetLeft());
+            insetsT = Math.max(insetsT, displayCutout.getSafeInsetTop());
+            insetsR = Math.max(insetsR, displayCutout.getSafeInsetRight());
+            insetsB = Math.max(insetsB, displayCutout.getSafeInsetBottom());
+          }
         }
-        final android.view.DisplayCutout displayCutout = insets.getDisplayCutout();
-        if (displayCutout != null) {
-          insetsL = Math.max(insetsL, displayCutout.getSafeInsetLeft());
-          insetsT = Math.max(insetsT, displayCutout.getSafeInsetTop());
-          insetsR = Math.max(insetsR, displayCutout.getSafeInsetRight());
-          insetsB = Math.max(insetsB, displayCutout.getSafeInsetBottom());
-        }
+      } catch (Exception e) {
+        // unsuported ?
+        // cause floating window
       }
-    } catch (Exception e) {
-      // unsuported ?
-      // cause floating window
+      insetNative(insetsL, insetsT, insetsR, insetsB);
+      return insets;
     }
-    insetNative(insetsL, insetsT, insetsR, insetsB);
-    return insets;
-  }
-*/
+  */
   @Override
   public void surfaceCreated(SurfaceHolder holder) {
     super.surfaceCreated(holder);
@@ -107,5 +107,5 @@ public class MainActivity extends NativeActivity /* implements View.OnApplyWindo
   }
 
   // c implementation
-  //native void insetNative(int left, int top, int right, int bottom);
+  // native void insetNative(int left, int top, int right, int bottom);
 }
