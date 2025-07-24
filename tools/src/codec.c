7a568121881ff735f53ca4b6c6a6d910ec9160e7
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-07-24 17:03:33 +0800

    fix tools test

--- tools/src/codec.c
+++ tools/src/codec.c
@@ -1,16 +1,19 @@
 #include "codec.h"
 
+#include <stdarg.h>
+#include <stdio.h>
+
 // **
 // defined for
 
 // ***/
-#define CODEC_ERROR_MSG_LEN 512
-char codec_error[CODEC_ERROR_MSG_LEN] = {};
 
 const char *codec_get_error(void) {
   return codec_error;
 }
 void codec_write_error(const char *x, ...) {
+  #define CODEC_ERROR_MSG_LEN 512
+  static char codec_error[CODEC_ERROR_MSG_LEN] = {};
   va_list args;
   va_start(args, x);
   vsnprintf(codec_error, CODEC_ERROR_MSG_LEN, x, args);
