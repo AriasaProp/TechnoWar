432f4e8fb5fb4801b5fecfd19b2d7f8c5a2cf3c2
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-07-24 09:04:20 +0000

    Reformatting Code Sources

--- tools/src/codec.c
+++ tools/src/codec.c
@@ -12,7 +12,7 @@ const char *codec_get_error(void) {
   return codec_error;
 }
 void codec_write_error(const char *x, ...) {
-  #define CODEC_ERROR_MSG_LEN 512
+#define CODEC_ERROR_MSG_LEN 512
   static char codec_error[CODEC_ERROR_MSG_LEN] = {};
   va_list args;
   va_start(args, x);
