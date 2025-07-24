6110c99161c8243d2380738b7bcd18bdae82a598
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-07-24 16:13:13 +0000

    Reformatting Code Sources

--- android/c/opengles_wrapper.c
+++ android/c/opengles_wrapper.c
@@ -1,10 +1,10 @@
 #include <android/native_window.h>
 
 #include "engine.h"
-#include "log.h"
-#include "util.h"
 #include "glad.h"
 #include "glad_egl.h"
+#include "log.h"
+#include "util.h"
 
 #include <inttypes.h>
 #include <stdio.h>
@@ -91,7 +91,7 @@ static struct androidGraphics {
   vec2 viewportSize; //
   vec2 screenSize;   //
   vec4 insets;
-  
+
   void *egllib, *gleslib;
 
 } *src = NULL;
