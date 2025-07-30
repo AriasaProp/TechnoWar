e130c73e6f95864048801e7f0c705df02b0ff103
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-07-31 00:04:18 +0800

    add animation : 00:04:18 07/31/25

--- core/src/game_logic_example.c
+++ core/src/game_logic_example.c
@@ -17,17 +17,17 @@ static float rands(void) {
 void game_init() {
   srand(time(0));
   for (int i = 0; i < MAX_BOX; ++i) {
-    vel[i].x = (2f * rands()) - 1f;
-    vel[i].y = (2f * rands()) - 1f;
-    float x = 2800f * rands(), y = 720f * rands();
-    rects[i * 4 + 0] = (vec2){x + 25f, y + 25f}; // Bottom-right
-    rects[i * 4 + 1] = (vec2){x + 25f, y - 25f}; // Top-right
-    rects[i * 4 + 2] = (vec2){x - 25f, y + 25f}; // Bottom-left
-    rects[i * 4 + 3] = (vec2){x - 25f, y - 25f}; // Top-left
+    vel[i].x = (2.f * rands()) - 1.f;
+    vel[i].y = (2.f * rands()) - 1.f;
+    float x = 2800.f * rands(), y = 720.f * rands();
+    rects[i * 4 + 0] = (vec2){x + 25.f, y + 25.f}; // Bottom-right
+    rects[i * 4 + 1] = (vec2){x + 25.f, y - 25.f}; // Top-right
+    rects[i * 4 + 2] = (vec2){x - 25.f, y + 25.f}; // Bottom-left
+    rects[i * 4 + 3] = (vec2){x - 25.f, y - 25.f}; // Top-left
   }
 }
 struct flat_vertex *game_update(unsigned int *l) {
-  l = MAX_BOX;
+  *l = MAX_BOX;
   for (int i = 0; i < MAX_BOX; ++i) {
     rects[i * 4 + 0].pos.x += vel[i].x;
     rects[i * 4 + 0].pos.y += vel[i].y;
@@ -38,13 +38,13 @@ struct flat_vertex *game_update(unsigned int *l) {
     rects[i * 4 + 3].pos.x += vel[i].x;
     rects[i * 4 + 3].pos.y += vel[i].y;
 
-    if ((rects[i * 4 + 3].pos.y < -50) ||
-        (rects[i * 4 + 3].pos.y > 700)) {
-      vel[i].y *= -1f;
+    if ((rects[i * 4 + 3].pos.y < -50.f) ||
+        (rects[i * 4 + 3].pos.y > 700.f)) {
+      vel[i].y *= -1.f;
     }
-    if ((rects[i * 4 + 3].pos.x < -100) ||
-        (rects[i * 4 + 3].pos.x > 3200)) {
-      vel[i].x *= -1f;
+    if ((rects[i * 4 + 3].pos.x < -100.f) ||
+        (rects[i * 4 + 3].pos.x > 3200.f)) {
+      vel[i].x *= -1.f;
     }
   }
   return rects;
