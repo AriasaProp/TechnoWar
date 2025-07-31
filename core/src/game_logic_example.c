72dd53e17b3f29c5d43eff742e41511bba90aacb
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-07-31 15:38:02 +0800

    upback : 15:38:01 07/31/25

--- core/src/game_logic_example.c
+++ core/src/game_logic_example.c
@@ -5,50 +5,73 @@
 #include "engine.h"
 #include "util.h"
 
-#define MAX_BOX 10
-
-vec2 vel[MAX_BOX] = {0};
-struct flat_vertex rects[MAX_BOX * 4] = {0};
-
-static float rands(void) {
-  return (float)rand() / (float)RAND_MAX;
-}
+struct box {
+  vec2 pos, vel;
+  float size;
+} *boxs = NULL;
+struct flat_vertex *rects = NULL;
+unsigned int max_box;
 
 void game_init() {
   srand(time(0));
-  for (int i = 0; i < MAX_BOX; ++i) {
-    vel[i].x = (2.f * rands()) - 1.f;
-    vel[i].y = (2.f * rands()) - 1.f;
-    float x = 2800.f * rands(), y = 720.f * rands();
-    rects[i * 4 + 0].pos = (vec2){x + 25.f, y + 25.f}; // Bottom-right
-    rects[i * 4 + 1].pos = (vec2){x + 25.f, y - 25.f}; // Top-right
-    rects[i * 4 + 2].pos = (vec2){x - 25.f, y + 25.f}; // Bottom-left
-    rects[i * 4 + 3].pos = (vec2){x - 25.f, y - 25.f}; // Top-left
+  max_box = 5 + (rand() % 10);
+  boxs = (struct box *)malloc(sizeof(struct box) * max_box);
+  rects = (struct flat_vertex *)calloc(sizeof(struct flat_vertex), max_box * 4);
+  vec2 sZ = global_engine.g.getScreenSize();
+  for (int i = 0; i < max_box; ++i) {
+    // velocity around 5 to -5
+    boxs[i].vel.x = (4.f * (float)rand() / (float)RAND_MAX) - 2.f;
+    boxs[i].vel.y = (4.f * (float)rand() / (float)RAND_MAX) - 2.f;
+    // size around 135 to 75 (square)
+    boxs[i].size = 75.0f + (60.f * rand());
+    // position around inside screen - size
+    boxs[i].pos.x = CLAMP(boxs[i].size, rand() * sZ.x, sZ.x - boxs[i].size);
+    boxs[i].pos.y = CLAMP(boxs[i].size, rand() * sZ.y, sZ.y - boxs[i].size);
   }
 }
 struct flat_vertex *game_update(unsigned int *l) {
-  *l = MAX_BOX;
-  for (int i = 0; i < MAX_BOX; ++i) {
-    rects[i * 4 + 0].pos.x += vel[i].x;
-    rects[i * 4 + 0].pos.y += vel[i].y;
-    rects[i * 4 + 1].pos.x += vel[i].x;
-    rects[i * 4 + 1].pos.y += vel[i].y;
-    rects[i * 4 + 2].pos.x += vel[i].x;
-    rects[i * 4 + 2].pos.y += vel[i].y;
-    rects[i * 4 + 3].pos.x += vel[i].x;
-    rects[i * 4 + 3].pos.y += vel[i].y;
-
-    if ((rects[i * 4 + 3].pos.y < -50.f) ||
-        (rects[i * 4 + 3].pos.y > 700.f)) {
-      vel[i].y *= -1.f;
+  *l = max_box;
+  vec2 sZ = global_engine.g.getScreenSize();
+  size_t i, j;
+  float bis2, distx, disty, mindist;
+  float bottom, top, left, right;
+  for (i = 0; i < max_box; ++i) {
+    // update motion
+    boxs[i].pos.x += boxs[i].vel.x;
+    boxs[i].pos.y += boxs[i].vel.y;
+    // collision detection + velocity update
+    bis2 = boxs[i].size / 2;
+    // detect with other box
+    for (j = 0; j < max_box; ++j) {
+      if (i == j)
+        continue;
+      distx = boxs[i].pos.x - boxs[j].pos.x;
+      disty = boxs[i].pos.y - boxs[j].pos.y;
+      mindist = bis2 + boxs[j].size / 2;
+      if (distx <= mindist && disty <= mindist) {
+        boxs[i].vel.x += boxs[j].vel.x * boxs[j].size / boxs[i].size;
+        boxs[i].vel.y += boxs[j].vel.y * boxs[j].size / boxs[i].size;
+      }
     }
-    if ((rects[i * 4 + 3].pos.x < -100.f) ||
-        (rects[i * 4 + 3].pos.x > 3200.f)) {
-      vel[i].x *= -1.f;
+    // detect with walls
+    if ((boxs[i].pos.x <= bis2) ||
+        (boxs[i].pos.x + bis2 >= sZ.x)) {
+      boxs[i].vel.x *= -1.0f;
     }
+    if ((boxs[i].pos.y <= bis2) ||
+        (boxs[i].pos.y + bis2 >= sZ.y)) {
+      boxs[i].vel.y *= -1.0f;
+    }
+    // draw
+    rects[i * 4 + 0].pos = (vec2){boxs[i].pos.x + bis2, boxs[i].pos.y + bis2}; // Bottom-right
+    rects[i * 4 + 1].pos = (vec2){boxs[i].pos.x + bis2, boxs[i].pos.y - bis2}; // Top-right
+    rects[i * 4 + 2].pos = (vec2){boxs[i].pos.x - bis2, boxs[i].pos.y + bis2}; // Bottom-left
+    rects[i * 4 + 3].pos = (vec2){boxs[i].pos.x - bis2, boxs[i].pos.y - bis2}; // Top-left
   }
   return rects;
 }
 void game_clean() {
   (void)0;
+  free(boxs);
+  free(rects);
 }
