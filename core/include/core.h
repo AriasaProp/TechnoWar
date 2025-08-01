47911074e7b7d4521949245c2e1bcdf1e8d70e62
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-08-01 16:22:44 +0000

    Reformatting Code Sources

--- core/include/core.h
+++ core/include/core.h
@@ -3,9 +3,9 @@
 
 // this special for mobile device
 extern void *core_stateSaved(void);
-extern unsigned int core_stateLength (void);
+extern unsigned int core_stateLength(void);
 extern void core_stateLoad(void *);
-// common loop 
+// common loop
 extern void Main_update();
 extern void Main_pause();
 extern void Main_term();
