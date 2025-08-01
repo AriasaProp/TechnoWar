b6682ad78a53699a51b4b8185873a320fa361b68
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-08-01 23:58:57 +0800

    update collision 6 : 23:58:57 08/01/25

--- android/c/extrasManager.c
+++ android/c/extrasManager.c
@@ -1,20 +1,21 @@
-#include "engine.h"
 #include "manager.h"
+#include "engine.h"
 
 #include <sys/resource.h>
 #include <time.h>
+#include <string.h>
 
 static void *androidExtras_timeStart() {
-  return (void *)clock();
+  return (void*) clock();
 }
 static float androidExtras_timeEnd(void *d) {
   return (float)(clock() - (clock_t)d) / (float)CLOCKS_PER_SEC;
 }
 
-void androidExtras_init(void) {
+void androidExtras_init (void) {
   global_engine.e.time_start_sec = androidExtras_timeStart;
   global_engine.e.time_end_sec = androidExtras_timeEnd;
 }
-void androidExtras_term(void) {
+void androidExtras_term (void) {
   memset(&global_engine.e, 0, sizeof(struct engine_extras));
 }
